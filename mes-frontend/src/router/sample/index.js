341ed956766587319d6bef8d1966c034cd47bdb4
awei <2069682479@qq.com>
2024-05-12 14:34:39 +0800

    新增前端架构

---
+++ mes-frontend/src/router/sample/index.js
@@ -0,0 +1,32 @@
+const routes = [
+  {
+    path: '/sample',
+    name: 'Sample',
+    redirect: { path: '/sample/file' },
+    component: () => import('@/views/sample/SampleView.vue'),
+    children: [
+      {
+        path: '/sample/file',
+        component: () => import('@/views/sample/file/FileUpload.vue')
+      },
+      {
+        path: '/sample/excel',
+        component: () => import('@/views/sample/excel/Excel.vue')
+      },
+      {
+        path: '/sample/print',
+        component: () => import('@/views/sample/print/Print.vue')
+      },
+      {
+        path: '/sample/editor',
+        component: () => import('@/views/sample/editor/Editor.vue')
+      },
+      {
+        path: '/sample/pdfobject',
+        component: () => import('@/views/sample/pdfobject/PdfObject.vue')
+      }
+    ]
+  }
+]
+
+export default routes
