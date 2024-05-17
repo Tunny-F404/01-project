f3180eb8fc8a31513819a43a413ad7f07b65dc3a
sky <1766976954@qq.com>
2024-05-17 22:33:56 +0800

    爱吃猫的鱼完成了自己负责的接口与领域模型的定义

---
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j2/orgstructure/JobApis.java
@@ -0,0 +1,20 @@
+package com.zeroone.star.project.j2.orgstructure;
+
+import com.zeroone.star.project.j2.orgstructure.dto.JobDTO;
+import com.zeroone.star.project.vo.ResultStatus;
+import org.springframework.http.ResponseEntity;
+
+public interface JobApis {
+
+    //添加岗位
+    ResultStatus addJob(JobDTO jobDTO);
+
+    //删除岗位
+    ResultStatus removeJob(Long[] ids);
+
+    //修改岗位
+    ResultStatus modifyJob(JobDTO jobDTO);
+
+    //导出岗位文件
+    ResponseEntity<byte[]> exportJob(String group, String storageId);
+}
---
+++ mes-java/mes-j2-orgstructure/src/main/java/com/zeroone/star/orgstructure/controller/JobController.java
@@ -0,0 +1,45 @@
+package com.zeroone.star.orgstructure.controller;
+
+import com.zeroone.star.project.j2.orgstructure.JobApis;
+import com.zeroone.star.project.j2.orgstructure.dto.JobDTO;
+import com.zeroone.star.project.vo.ResultStatus;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequestMapping("job")
+@Api(tags = "岗位控制器")
+public class JobController implements JobApis {
+    @Override
+    @PostMapping("add-job")
+    @ApiOperation("新增岗位")
+    public ResultStatus addJob(@RequestBody JobDTO jobDTO) {
+        return ResultStatus.SUCCESS;
+    }
+
+    @Override
+    @DeleteMapping("remove-job")
+    @ApiOperation("删除岗位")
+    public ResultStatus removeJob(@RequestParam Long[] ids) {
+        return ResultStatus.SUCCESS;
+    }
+
+    @Override
+    @PostMapping("modify-job")
+    @ApiOperation("修改岗位")
+    public ResultStatus modifyJob(@RequestBody JobDTO jobDTO) {
+        return ResultStatus.SUCCESS;
+    }
+
+    @Override
+    @GetMapping("export-job")
+    @ApiOperation("导出岗位")
+    public ResponseEntity<byte[]> exportJob(String group, String storageId) {
+        return null;
+    }
+
+}
