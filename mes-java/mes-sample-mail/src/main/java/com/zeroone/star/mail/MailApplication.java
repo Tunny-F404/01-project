66a7a8de5d7e3ae16f22688c2c0f7ccc00b5293c
awei <2069682479@qq.com>
2024-05-17 10:52:13 +0800

    新增Mail使用示例模块

---
+++ mes-java/mes-sample-mail/src/main/java/com/zeroone/star/mail/MailApplication.java
@@ -0,0 +1,22 @@
+package com.zeroone.star.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 描述：服务器启动入口
+ * </p>
+ * <p>版权：&copy;01星球</p>
+ * <p>地址：01星球总部</p>
+ * @author 阿伟学长
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class MailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailApplication.class, args);
+    }
+
+}
