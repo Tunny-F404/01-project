bdc477dd39017d6a850c9619259a80a77a11cd95
diwuJake <131629444+diwuJake@users.noreply.github.com>
2024-05-19 01:17:22 +0800

    添加了关于查询登录日志，添加登录日志的API 定义

--- mes-java/mes-apis/src/main/java/com/zeroone/star/project/j4/sysmanagement/logmanagement/LoginLogApis.java
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j4/sysmanagement/logmanagement/LoginLogApis.java
@@ -1,9 +1,27 @@
 package com.zeroone.star.project.j4.sysmanagement.logmanagement;
 
+import com.zeroone.star.project.dto.PageDTO;
+import com.zeroone.star.project.dto.j4.logmanagement.SysLoginInfoDTO;
+import com.zeroone.star.project.query.j4.logmanagement.SysLoginInfoQuery;
+import com.zeroone.star.project.vo.JsonVO;
+
 /**
  * 登录日志接口API定义
  * @author: 酌
  * @version 1.0
  */
 public interface LoginLogApis {
+    /**
+     * 获取登录日志的信息列表
+     * @param query  查询条件
+     * @return
+     */
+    JsonVO<PageDTO<SysLoginInfoDTO>> querySysLoginInfos(SysLoginInfoQuery query);
+
+    /**
+     * 添加登录日志信息
+     * @param dto 登录日志信息对象
+     * @return
+     */
+    JsonVO<Boolean> addSysLoginInfo(SysLoginInfoDTO dto);
 }
---
+++ mes-java/mes-j4-sysmanagement/src/main/java/com/zeroone/star/sysmanagement/controller/SysLoginInfoController.java
@@ -0,0 +1,10 @@
+package com.zeroone.star.sysmanagement.controller;
+
+/**
+ * @Description 登录日志信息管理
+ * @Author diwu
+ * @Date 2024/5/19
+ */
+public class SysLoginInfoController {
+
+}
