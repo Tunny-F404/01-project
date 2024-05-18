c5799a3b0e9d7cf7d1531a17296fd863d89ea948
hejianwei <1324700730@qq.com>
2024-05-19 00:15:29 +0800

    提交了对应DTO/Query/VO部分的代码，初步测试没有异常报错，添加到了controller控制器上

---
+++ mes-cpp/mes-c2-repairorder/domain/dto/repaircontent/RepaircontentDTO.h
@@ -0,0 +1,53 @@
+#pragma once
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: hejianwei
+ @Date: 2024/05/18 23:14
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#ifndef _REPAIRCONTENT_DTO_
+#define _REPAIRCONTENT_DTO_
+#include "../../GlobalInclude.h"
+
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+/**
+ * 维修内容传输对象
+ */
+class RepaircontentDTO : public oatpp::DTO
+{
+	DTO_INIT(RepaircontentDTO, DTO);
+	
+	//项目名称
+	API_DTO_FIELD(String, projectName, ZH_WORDS_GETTER("repaircontent.field.name"), true, "");
+	//故障描述
+	API_DTO_FIELD(String, errorInstance, ZH_WORDS_GETTER("repaircontent.field.instance"), true, "");
+	//故障资源描述
+	API_DTO_FIELD(String, errorInstanceSource, ZH_WORDS_GETTER("repaircontent.field.source"), true, "");
+	//维修情况
+	API_DTO_FIELD(String, repairStatus, ZH_WORDS_GETTER("repaircontent.field.status"), true, "");
+
+};
+
+/**
+ * 维修内容分页传输对象
+ */
+class RepaircontentPageDTO : public PageDTO<RepaircontentDTO::Wrapper>
+{
+	DTO_INIT(RepaircontentPageDTO, PageDTO<RepaircontentDTO::Wrapper>);
+};
+
+#include OATPP_CODEGEN_END(DTO)
+#endif // !_REPAIRCONTENT_DTO_
