28921de47215e20d311c89cfe72b93c5070f00d9
灵魂 <yusnow27@163.com>
2024-05-18 20:11:01 +0800

    删除多余内容

--- mes-cpp/arch-sample/controller/dept/DeptController.cpp
+++
@@ -1,7 +0,0 @@
-#include "stdafx.h"
-#include "DeptController.h"
-
-DeptTableJsonVO::Wrapper DeptController::execDeptTable(const DeptQuery::Wrapper& query)
-{
-	return DeptTableJsonVO::Wrapper();
-}
