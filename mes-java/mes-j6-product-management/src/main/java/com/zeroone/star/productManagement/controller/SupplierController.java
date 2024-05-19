3328c4dd7ae1687bc158dec6adfcdaebd35c6852
“s8x” <xbc20030512@163.com>
2024-05-19 22:14:42 +0800

    添加商品管理模块关联供应商接口定义及模型类。

--- mes-java/mes-j6-product-management/src/main/java/com/zeroone/star/productManagement/controller/SupplierController.java
+++ mes-java/mes-j6-product-management/src/main/java/com/zeroone/star/productManagement/controller/SupplierController.java
@@ -7,39 +7,39 @@ import com.zeroone.star.project.j6.product_management.dto.SupplierListDTO;
 import com.zeroone.star.project.j6.product_management.query.SupplierListQuery;
 import com.zeroone.star.project.j6.product_management.vo.SupplierVO;
 import com.zeroone.star.project.vo.JsonVO;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/product-management/suppliers")
-@ApiModel(description = "关联供应商相关接口")
+@Api(tags = "关联供应商相关接口")
 public class SupplierController implements SupplierApis {
     @GetMapping("/list")
-    @ApiModelProperty(value = "查询关联供应商列表", notes = "根据查询条件查询关联供应商列表")
+    @ApiOperation(value = "查询关联供应商列表")
     @Override
     public JsonVO<List<SupplierVO>> querySupplierList(SupplierListQuery query) {
         return null;
     }
 
     @PostMapping("/add")
-    @ApiModelProperty(value = "添加关联供应商", notes = "添加多个关联供应商")
+    @ApiOperation(value = "添加关联供应商")
     @Override
     public JsonVO<String> addSupplierList(@RequestBody SupplierListDTO dto) {
         return null;
     }
 
     @PostMapping("/update")
-    @ApiModelProperty(value = "更新关联供应商", notes = "更新关联供应商")
+    @ApiOperation(value = "更新关联供应商")
     @Override
     public JsonVO<String> updateSupplier(SupplierDTO dto) {
         return null;
     }
 
     @PostMapping("/delete")
-    @ApiModelProperty(value = "删除关联供应商", notes = "删除关联供应商")
+    @ApiOperation(value = "删除关联供应商")
     @Override
     public JsonVO<String> deleteBatchSupplier(ProductSupplierListDTO dto) {
         return null;
