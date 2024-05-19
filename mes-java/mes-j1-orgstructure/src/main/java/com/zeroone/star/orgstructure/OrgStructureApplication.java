a7faded06c313c4af5b2620c41ddf883dea59da9
Pluto <2502664750@qq.com>
2024-05-20 06:57:37 +0800

    包名修改

--- mes-java/mes-j1-orgstructure/src/main/java/com/zeroone/star/orgstructure/OrgStructureApplication.java
+++ mes-java/mes-j1-orgstructure/src/main/java/com/zeroone/star/orgstructure/OrgStructureApplication.java
@@ -1,7 +1,8 @@
-package com.zeroone.star.mycenter;
+package com.zeroone.star.orgstructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
