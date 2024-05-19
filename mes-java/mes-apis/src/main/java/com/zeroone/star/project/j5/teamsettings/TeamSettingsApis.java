dcdc95e35c576885427e8cd24e732c5ab8a3b302
ChengcanGuo <2488700263@qq.com>
2024-05-19 21:33:33 +0800

    给每个接口注释说明

--- mes-java/mes-apis/src/main/java/com/zeroone/star/project/j5/teamsettings/TeamSettingsApis.java
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j5/teamsettings/TeamSettingsApis.java
@@ -1,4 +1,13 @@
 package com.zeroone.star.project.j5.teamsettings;
 
+/**
+ * <p>
+ * 描述：班组设置接口
+ * </p>
+ * <p>版权：&copy;01星球</p>
+ * <p>地址：01星球总部</p>
+ * @author interstellar
+ * @version 1.0.0
+ */
 public interface TeamSettingsApis {
 }
