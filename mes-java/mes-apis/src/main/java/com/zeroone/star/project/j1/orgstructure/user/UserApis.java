bde65c903018e8081d61ad6df8209505f3187cee
Pluto <2502664750@qq.com>
2024-05-20 06:55:35 +0800

    用户增加、修改、删除的邻域模型与接口

--- mes-java/mes-apis/src/main/java/com/zeroone/star/project/j1/orgstructure/user/UserApis.java
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j1/orgstructure/user/UserApis.java
@@ -1,5 +1,8 @@
 package com.zeroone.star.project.j1.orgstructure.user;
 
+import com.zeroone.star.project.j1.orgstructure.query.user.UserQuery;
+import com.zeroone.star.project.vo.JsonVO;
+
 /**
  * <p>
  *     描述：用户管理接口
@@ -7,5 +10,25 @@ package com.zeroone.star.project.j1.orgstructure.user;
  * @author twelve
  * @version 1.0.0
  */
-public class UserApis {
+public interface UserApis {
+    /**
+     * 新增用户
+     * @param userQuery 新增用户信息
+     * @return 返回信息
+     */
+    JsonVO<String> addUser(UserQuery userQuery);
+
+    /**
+     * 更新用户信息
+     * @param userQuery 更新用户信息
+     * @return 返回信息
+     */
+    JsonVO<String> updateUser(UserQuery userQuery);
+
+    /**
+     * 删除用户
+     * @param userIds 用户ID
+     * @return 返回信息
+     */
+    JsonVO<String> deleteUser(Long[] userIds);
 }
--- mes-java/mes-j1-orgstructure/src/main/java/com/zeroone/star/orgstructure/controller/UserController.java
+++ mes-java/mes-j1-orgstructure/src/main/java/com/zeroone/star/orgstructure/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.zeroone.star.orgstructure.controller;
 
-public class UserController {
+import com.zeroone.star.project.j1.orgstructure.query.user.UserQuery;
+import com.zeroone.star.project.j1.orgstructure.user.UserApis;
+import com.zeroone.star.project.vo.JsonVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("user")
+@Api(tags = "用户管理")
+public class UserController implements UserApis {
+    @PostMapping("add-user")
+    @ApiOperation(value = "新增用户")
+    @Override
+    public JsonVO<String> addUser(UserQuery userQuery) {
+        return null;
+    }
+
+    @PutMapping("update-user")
+    @ApiOperation(value = "修改用户")
+    @Override
+    public JsonVO<String> updateUser(UserQuery userQuery) {
+        return null;
+    }
+
+    @DeleteMapping("delete-user")
+    @ApiOperation(value = "删除用户")
+    @Override
+    public JsonVO<String> deleteUser(Long[] userIds) {
+        return null;
+    }
 }
