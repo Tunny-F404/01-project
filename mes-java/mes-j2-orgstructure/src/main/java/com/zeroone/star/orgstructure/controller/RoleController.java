f04ad88b68708b7af14e24f8ee056c5c76108aca
0xu0 <1393068976@qq.com>
2024-05-19 10:01:02 +0800

    修改了RoleDto，添加了数据库中所有字段 在RoleController添加了方法 添加了RoleApi

--- mes-java/mes-j2-orgstructure/src/main/java/com/zeroone/star/orgstructure/controller/RoleController.java
+++ mes-java/mes-j2-orgstructure/src/main/java/com/zeroone/star/orgstructure/controller/RoleController.java
@@ -3,6 +3,7 @@ package com.zeroone.star.orgstructure.controller;
 
 import com.zeroone.star.project.dto.PageDTO;
 import com.zeroone.star.project.j2.orgstructure.dto.role.RoleDTO;
+import com.zeroone.star.project.j2.orgstructure.role.RoleApis;
 import com.zeroone.star.project.vo.JsonVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -21,7 +22,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/orgstructure/role")
 @Api(tags = "角色控制器")
-public class RoleController {
+public class RoleController implements RoleApis {
     /**
      * 获取角色名称列表
      */
@@ -57,5 +58,53 @@ public class RoleController {
     public JsonVO<List<RoleDTO>> modifyRole(@RequestParam Integer id) {
         return JsonVO.success(null);
     }
+    /**
+     * 添加角色
+     * @author 0xu0
+     * @param roleDTO
+     * @return
+     */
+    @PostMapping("add-Role")
+    @ApiOperation("添加角色")
+    public JsonVO<RoleDTO> addRole(@RequestBody RoleDTO roleDTO){
+        return JsonVO.success(roleDTO);
+    }
+
+    /***
+     * 修改角色状态（角色页面，有个开关符号，控制停用和启用）
+     * @author 0xu0
+     * @param status
+     * @return
+     */
+    @PostMapping("modify-RoleStatus")
+    @ApiOperation("修改角色状态")
+    public JsonVO<String> modifyRoleStatus(@RequestParam String status){
+        return JsonVO.success(status);
+    }
+
+
+    /**
+     * 删除角色，支持批量删除
+     * @author 0xu0
+     * @param id
+     * @return
+     */
+    @DeleteMapping("delete-Roles")
+    @ApiOperation("删除角色")
+    public JsonVO<Long[]> deleteRoles(@RequestParam Long[] id){
+        return JsonVO.success(id);
+    }
+
+    /**
+     * 修改角色信息
+     * @param roleDTO
+     * @return
+     */
+    @PostMapping("modify-RoleInfo")
+    @ApiOperation("修改角色信息")
+    public JsonVO<RoleDTO> modifyRoleInfo(@RequestBody RoleDTO roleDTO){
+        return JsonVO.success(roleDTO);
+    }
+
 }
 
