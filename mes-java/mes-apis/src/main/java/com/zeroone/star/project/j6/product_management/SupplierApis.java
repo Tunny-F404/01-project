05a898618371244b3190695d67b18339f56adc1c
“s8x” <xbc20030512@163.com>
2024-05-19 21:31:04 +0800

    api

---
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j6/product_management/SupplierApis.java
@@ -0,0 +1,11 @@
+package com.zeroone.star.project.j6.product_management;
+
+
+import com.zeroone.star.project.vo.JsonVO;
+
+public interface SupplierApis {
+    JsonVO getSupplierList();
+    JsonVO addSupplierList();
+    JsonVO updateSupplier();
+    JsonVO deleteBatchSupplier();
+}
