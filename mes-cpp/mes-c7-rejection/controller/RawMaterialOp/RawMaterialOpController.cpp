667e2e21ee2d2a163571af2788cb444075202028
betterter <141387243+betterter@users.noreply.github.com>
2024-05-20 01:32:41 +0800

    完成了退料订单删除和退料订单导出接口，暂时不知道如何实现退料执行

---
+++ mes-cpp/mes-c7-rejection/controller/RawMaterialOp/RawMaterialOpController.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "RawMaterialOpController.h"
+#include "../../service/RawMaterialOpService/RawMaterialOpService.h"
+#include "../ApiDeclarativeServicesHelper.h"
+
+
+
+Uint64JsonVO::Wrapper RawMaterialOpController::execRemoveRawMaterialOp(const UInt64& id)
+{
+	return Uint64JsonVO::Wrapper();
+}
+
+std::shared_ptr<oatpp::web::server::api::ApiController::OutgoingResponse> RawMaterialOpController::execDownloadFile(const String& filename)
+{
+	// 构建文件全路径
+	std::string fullPath = "public/static/" + URIUtil::urlDecode(filename.getValue(""));
+	// 读取文件
+	auto fstring = String::loadFromFile(fullPath.c_str());
+
+	// 判断是否读取成功
+	if (!fstring)
+	{
+		std::cerr << "Failed to open file: " << std::strerror(errno) << std::endl;
+		return createResponse(Status::CODE_404, "File Not Found");
+	}
+
+	// 创建响应头
+	auto response = createResponse(Status::CODE_200, fstring);
+
+	// 设置响应头信息
+	response->putHeader("Content-Disposition", "attachment; filename=" + filename.getValue(""));
+	// 影响成功结果
+	return response;
+}
---
+++ mes-cpp/mes-c7-rejection/controller/RawMaterialOp/RawMaterialOpController.h
@@ -0,0 +1,56 @@
+#pragma once
+#ifndef _RawMaterialOp_CONTROLLER_
+#define _RawMaterialOp_CONTROLLER_
+
+#include "domain/vo/BaseJsonVO.h"
+#include "ServerInfo.h"
+#include "domain/dto/RawMaterialOp/RawMaterialOpDTO.h"
+#include "domain/query/RawMaterialOpQuery.h"
+#include "domain/vo/RawMaterialOpVO.h"
+#include "ApiHelper.h"
+
+// 0 定义API控制器使用宏
+#include OATPP_CODEGEN_BEGIN(ApiController) //<- Begin
+
+/**
+ * 示例控制器，演示基础接口的使用
+ */
+class RawMaterialOpController : public oatpp::web::server::api::ApiController // 1 继承控制器
+{
+	// 2 定义控制器访问入口
+	API_ACCESS_DECLARE(RawMaterialOpController);
+	// 3 定义接口
+public:
+
+	// 定义删除单据接口描述
+	ENDPOINT_INFO(removeRawMaterialOp)
+	{
+		// 定义标题和返回类型以及授权支持
+		API_DEF_ADD_COMMON_AUTH(ZH_WORDS_GETTER("RawMaterialOp.delete.summary"), Uint64JsonVO::Wrapper);
+		// 定义其他路径参数说明
+		API_DEF_ADD_PATH_PARAMS(UInt64, "id", ZH_WORDS_GETTER("RawMaterialOp.field.id"), 1, true);
+	}
+	//定义删除单据接口处理
+	API_HANDLER_ENDPOINT_AUTH(API_M_DEL, "/RawMaterialOp/{id}", removeRawMaterialOp, PATH(UInt64, id), execRemoveRawMaterialOp(id));
+
+
+	// 定义文件下载接口
+	ENDPOINT_INFO(downloadFile) {
+		API_DEF_ADD_COMMON(ZH_WORDS_GETTER("RawMaterialOp.file.download.summary"), Void);
+		API_DEF_ADD_QUERY_PARAMS(String, "filename", ZH_WORDS_GETTER("RawMaterialOp.file.download.filename"), "file/test.jpg", true);
+	}
+	// 定义端点
+	ENDPOINT(API_M_GET, "/file/download", downloadFile, QUERY(String, filename)) {
+		return execDownloadFile(filename);
+	}
+private:
+	// 删除生产退料单行列表（支持批量删除）
+	Uint64JsonVO::Wrapper execRemoveRawMaterialOp(const UInt64& id);
+
+	// 执行文件下载处理
+	std::shared_ptr<OutgoingResponse> execDownloadFile(const String& filename);
+};
+
+// 0 取消API控制器使用宏
+#include OATPP_CODEGEN_END(ApiController) //<- End Codegen
+#endif // _SAMPLE_CONTROLLER_
