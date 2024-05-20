fb0ce562dc7428a55d126fc796e6e9faedadf083
3034909308@qq.com <3034909308@qq.com>
2024-05-20 09:22:14 +0800

    更新了domain领域模型的路径，优化了包名

--- mes-java/mes-domain/src/main/java/com/zeroone/star/project/j5/dto/scheduleplan/SchPlanDto.java
+++ mes-java/mes-domain/src/main/java/com/zeroone/star/project/j5/dto/scheduleplan/SchPlanDto.java
@@ -1,4 +1,4 @@
-package com.zeroone.star.project.j5.dto;
+package com.zeroone.star.project.j5.dto.scheduleplan;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
--- mes-java/mes-domain/src/main/java/com/zeroone/star/project/j5/vo/scheduleplan/SchPlanUpdateVO.java
+++ mes-java/mes-domain/src/main/java/com/zeroone/star/project/j5/vo/scheduleplan/SchPlanUpdateVO.java
@@ -1,4 +1,4 @@
-package com.zeroone.star.project.j5.vo;
+package com.zeroone.star.project.j5.vo.scheduleplan;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
