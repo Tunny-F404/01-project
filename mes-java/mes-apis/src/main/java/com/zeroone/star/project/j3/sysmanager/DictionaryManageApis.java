3350d98c2977c29639a4e66cbfe6f55ee7fc2b1d
Ankh <1214959101@qq.com>
2024-05-20 17:56:01 +0800

    1. 删除不必要的Query(Query只有一个属性不需要定义) 2. 为dto添加数据约束 3. 新增字典操作接口 4. 根据新增接口编写Controller

--- mes-java/mes-apis/src/main/java/com/zeroone/star/project/j3/sysmanager/DictionaryManageApis.java
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j3/sysmanager/DictionaryManageApis.java
@@ -1,8 +1,36 @@
 package com.zeroone.star.project.j3.sysmanager;
 
 
+import com.zeroone.star.project.j3.dto.InsertDictTypeDTO;
+import com.zeroone.star.project.j3.vo.DictDataNameVO;
+import com.zeroone.star.project.j3.vo.DictTypeNameVO;
+import com.zeroone.star.project.vo.JsonVO;
+
+import java.util.List;
+
 /**
  * 系统管理-字典管理相关接口
  */
 public interface DictionaryManageApis {
+
+    /**
+     * 新增字典(dict_type)
+     * @param dto 新增数据
+     * @return 新增数据的唯一编号
+     */
+    JsonVO<Long> addDictType(InsertDictTypeDTO dto);
+
+    /**
+     * 根据给定的字典类型获取对应的字典数据名称列表
+     * @param dictType 字典类型, 例如sys_user_sex
+     * @return 对应的字典数据名称列表
+     */
+    JsonVO<List<DictDataNameVO>> queryDictDataNamesByDictType(String dictType);
+
+    /**
+     * 获取字典名称列表
+     * @return 字典名称列表
+     */
+    JsonVO<List<DictTypeNameVO>> queryDictTypeNames();
+
 }
--- mes-java/mes-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/DictionaryManageController.java
+++ mes-java/mes-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/DictionaryManageController.java
@@ -1,6 +1,42 @@
 package com.zeroone.star.sysmanager.controller;
 
+import com.zeroone.star.project.j3.dto.InsertDictTypeDTO;
 import com.zeroone.star.project.j3.sysmanager.DictionaryManageApis;
+import com.zeroone.star.project.j3.vo.DictDataNameVO;
+import com.zeroone.star.project.j3.vo.DictTypeNameVO;
+import com.zeroone.star.project.vo.JsonVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@RestController
+@RequestMapping("sys-manager/dictionary-manager")
+@Api(tags = "字典管理")
+@Validated
 public class DictionaryManageController implements DictionaryManageApis {
+
+    @ApiOperation(value = "新增字典类别")
+    @PostMapping("add-dictType")
+    @Override
+    public JsonVO<Long> addDictType(@RequestBody InsertDictTypeDTO dto) {
+        return null;
+    }
+
+    @ApiOperation(value = "根据字典类型获取字典数据名称列表")
+    @GetMapping("query-dictDateNames-by-dictType")
+    @Override
+    public JsonVO<List<DictDataNameVO>> queryDictDataNamesByDictType(@NotBlank(message = "字典类型不能为空") String dictType) {
+        return null;
+    }
+
+    @ApiOperation(value = "获取字典名称列表")
+    @GetMapping("query-dictTypeNames")
+    @Override
+    public JsonVO<List<DictTypeNameVO>> queryDictTypeNames() {
+        return null;
+    }
 }
