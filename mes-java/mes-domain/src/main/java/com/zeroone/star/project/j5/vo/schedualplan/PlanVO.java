5641b67ca4dc421e034ce10a8ad061f8f54401ae
Liao Yan <liaoxueyan030106@gmail.com>
2024-05-20 22:46:51 +0800

    排班计划列表显示数据（暂时根据网页响应给前端的数据，全部封装到一个VO）

--- mes-java/mes-domain/src/main/java/com/zeroone/star/project/j5/vo/schedualplan/PlanVO.java
+++ mes-java/mes-domain/src/main/java/com/zeroone/star/project/j5/vo/schedualplan/PlanVO.java
@@ -1,12 +1,10 @@
 package com.zeroone.star.project.j5.vo.schedualplan;
 
-import com.zeroone.star.project.vo.login.MenuTreeVO;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.LocalDate;
-import java.util.List;
 
 /**
  * <p>
@@ -19,9 +17,13 @@ import java.util.List;
  */
 @Data
 @ApiModel("查询返回排班计划列表数据对象")
-public class PlanQueryVO {
+public class PlanVO {
+
+    @ApiModelProperty(value = "计划ID", example = "1")
+    private Long planId;
+
     @ApiModelProperty(value = "计划编号", example = "PL12345")
-    private String planNumber;     // 计划编号
+    private String planCode;     // 计划编号
 
     @ApiModelProperty(value = "计划名称", example = "年度计划")
     private String planName;       // 计划名称
@@ -39,9 +41,45 @@ public class PlanQueryVO {
     private String shiftType;      // 轮班方式（如白班、两班倒）
 
     @ApiModelProperty(value = "倒班方式", example = "按月")
-    private String rotationType;   // 倒班方式（如按月、按天）
+    private String shiftMethod;   // 倒班方式（如按月、按天）
+
+    @ApiModelProperty(value = "数", example = "1")
+    private Integer shiftCount;
 
     @ApiModelProperty(value = "单据状态", example = "已确认")
     private String status;         // 单据状态（已确认、草稿）
+
+    @ApiModelProperty(value = "创建者", example = "已确认")
+    private String createBy;
+
+    @ApiModelProperty(value = "创建时间", example = "已确认")
+    private LocalDate createTime;
+
+    @ApiModelProperty(value = "更新者", example = "已确认")
+    private String updateBy;
+
+    @ApiModelProperty(value = "更新时间", example = "已确认")
+    private LocalDate updateTime;
+
+    @ApiModelProperty(value = "预留字段1", example = "null")
+    private String attr1;
+
+    @ApiModelProperty(value = "预留字段2", example = "null")
+    private String attr2;
+
+    @ApiModelProperty(value = "预留字段3", example = "0")
+    private Integer attr3;
+
+    @ApiModelProperty(value = "预留字段4", example = "0")
+    private Integer attr4;
+
+    @ApiModelProperty(value = "备注", example = "备注")
+    private String remark;
+
+    @ApiModelProperty(value = "班组类型", example = "CNC")
+    private String calendarType;
+//    "searchValue": null,
+//    "params": {},
+
 
 }
