5f5f08a626a4a8021c91bc75a8f0350ed32f3c68
“dataclover” <3123595411@qq.com>
2024-05-21 00:08:59 +0800

    更新MdItemApis注释

--- mes-java/mes-apis/src/main/java/com/zeroone/star/project/j6/product_management/MdItemApis.java
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j6/product_management/MdItemApis.java
@@ -5,10 +5,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface MdItemApis {
-    //导出物料产品列表
+    /**
+     * 导出物料产品列表
+     * @param mdItemQuery 查询参数
+     * @return Excel 文件内容
+     */
     ResponseEntity<byte[]> exportToExcel(MdItemQuery mdItemQuery);
-    //下载导入模板
+    /**
+     * 下载导入模板
+     * @return 导入模板文件内容
+     */
     ResponseEntity<byte[]> importTemplate();
-    //导入物料产品数据
+    /**
+     * 导入物料产品数据
+     * @param file Excel 文件
+     * @param updateSupport 是否更新已经存在的用户数据
+     * @return 导入结果
+     */
     ResponseEntity<String> importFromExcel(MultipartFile file, boolean updateSupport);
 }
