2bd75080e757ae43e663b8dbe8c9303f284ac608
XiaoYe <2760999380@qq.com>
2024-05-20 11:03:48 +0800

    合并整合角色分支

---
+++ mes-java/mes-j2-sysmanagement/src/main/java/com/zeroone/star/sysManagement/controller/MenuController.java
@@ -0,0 +1,43 @@
+package com.zeroone.star.sysmanagement.controller;
+
+import com.zeroone.star.project.j2.sysManagement.menu.MenuApis;
+
+import com.zeroone.star.project.j2.sysmanagent.dto.menu.MenuDTO;
+import com.zeroone.star.project.vo.JsonVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author  熠
+ * @version 1.0.0
+ * @Description: 测试控制器
+ */
+@RestController
+@RequestMapping("sys")
+@Api(tags = "测试控制器")
+public class MenuController implements MenuApis {
+
+
+    @PostMapping("add-resource")
+    @ApiOperation("新增资源")
+    @Override
+    public JsonVO<MenuDTO> addResource(@RequestBody MenuDTO sysmanagentDTO) {
+        return JsonVO.success(sysmanagentDTO);
+    }
+
+    @PostMapping("remove-resource")
+    @ApiOperation("删除资源")
+    @Override
+    public JsonVO<Integer> removeResource(String id) {
+        return JsonVO.success(1);
+    }
+
+
+    @PostMapping("modify-resource")
+    @ApiOperation("修改资源")
+    @Override
+    public JsonVO<Integer> modifyResource(MenuDTO resourceDTO) {
+        return null;
+    }
+}
