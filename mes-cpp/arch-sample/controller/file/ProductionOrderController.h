42124772f297033b434b7cfc2161cc60a0e02f4d
Lorraine Wen <2145901423@qq.com>
2024-05-20 23:16:19 +0800

    还没写完

---
+++ mes-cpp/arch-sample/controller/file/ProductionOrderController.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: awei
+ @Date: 2022/10/25 0:27:04
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#ifndef _BOMORDER_CONTROLLER_
+#define _BOMORDER_CONTROLLER_
+
+#include "domain/vo/BaseJsonVO.h"
+#include "domain/dto/productionorder/ExportBomOrderDTO.h"
+#include "domain/vo/productionorder/ExportBomOrderVO.h"
+
+// 0 定义API控制器使用宏
+#include OATPP_CODEGEN_BEGIN(ApiController) //<- Begin Codegen
+
+/**
+ * 示例控制器，演示基础接口的使用
+ */
+class ProductionOrderController : public oatpp::web::server::api::ApiController // 1 继承控制器
+{
+	API_ACCESS_DECLARE(ProductionOrderController);
+public:
+
+private:
+};
+
+// 0 取消API控制器使用宏
+#include OATPP_CODEGEN_END(ApiController) //<- End Codegen
+#endif // _BOMORDER_CONTROLLER_
---
+++ mes-cpp/arch-sample/domain/query/productionorder/ExportOrderQuery.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#ifndef _Order_QUERY_
+#define _Order_QUERY_
+#include "../../GlobalInclude.h"
+#include "domain/query/PageQuery.h"//有分页就导入并继承
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+class OrderQuery : public oatpp::DTO
+{
+	DTO_INIT(OrderQuery, DTO);
+	API_DTO_FIELD_DEFAULT(String, workOrderCode,ZH_WORDS_GETTER("productionorder.query.fields.workorder_code"));
+	API_DTO_FIELD_DEFAULT(String, workOrderName, ZH_WORDS_GETTER("productionorder.query.fields.workorder_name"));
+	API_DTO_FIELD_DEFAULT(String, sourceCode, ZH_WORDS_GETTER("productionorder.query.fields.source_code"));
+	API_DTO_FIELD_DEFAULT(String, productCode, ZH_WORDS_GETTER("productionorder.query.fields.product_code"));
+	API_DTO_FIELD_DEFAULT(String, productName, ZH_WORDS_GETTER("productionorder.query.fields.product_name"));
+	API_DTO_FIELD_DEFAULT(String, clientCode, ZH_WORDS_GETTER("productionorder.query.fields.client_code"));
+	API_DTO_FIELD_DEFAULT(String, clientName, ZH_WORDS_GETTER("productionorder.query.fields.client_name"));
+	API_DTO_FIELD_DEFAULT(String, requestDate, ZH_WORDS_GETTER("productionorder.query.fields.request_date"));
+};
+
+#include OATPP_CODEGEN_END(DTO)
+#endif // !_Order_QUERY_
