e90a4e5791c94c5c72c0cf2174b823443cc98354
ChengcanGuo <2488700263@qq.com>
2024-05-20 11:13:20 +0800

    写了班组列表查询对象

---
+++ mes-java/mes-domain/src/main/java/com/zeroone/star/project/j5/dto/teamsettings/TeamSettingsListDTO.java
@@ -0,0 +1,13 @@
+package com.zeroone.star.project.j5.dto.teamsettings;
+
+/**
+ * <p>
+ * 描述：查询然后响应的的班组列表
+ * </p>
+ * <p>版权：&copy;01星球</p>
+ * <p>地址：01星球总部</p>
+ * @author interstellar
+ * @version 1.0.0
+ */
+public class TeamSettingsListDTO {
+}
