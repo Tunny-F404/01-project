ab6225164b4bc51bb951b87cc226151b49d6ba8f
matao <1251281430@qq.com>
2024-05-20 20:06:05 +0800

    完成了UserQuery的编写

--- mes-java/mes-domain/src/main/java/com/zeroone/star/project/j1/orgstructure/query/user/UserQuery.java
+++ mes-java/mes-domain/src/main/java/com/zeroone/star/project/j1/orgstructure/query/user/UserQuery.java
@@ -1,4 +1,28 @@
 package com.zeroone.star.project.j1.orgstructure.query.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * <p>版权：&copy;01星球</p>
+ * <p>地址：01星球总部</p>
+ * @author Tme
+ * @version 1.0.0
+ */
+@Data
+@ApiModel("用户列表查询对象")
 public class UserQuery {
+    @ApiModelProperty(value = "用户名称", example = "admin")
+    private String userName;
+    @ApiModelProperty(value = "手机号码", example = "18888888888")
+    private String phonenumber;
+    @ApiModelProperty(value = "用户账号状态(0:正常 1:停用)", example = "1")
+    private String status;
+    @ApiModelProperty(value = "创建时间：开始日期", example = "2024-05-20")
+    private Date beginDate;
+    @ApiModelProperty(value = "创建时间：结束日期", example = "2024-05-21")
+    private Date endDate;
 }
