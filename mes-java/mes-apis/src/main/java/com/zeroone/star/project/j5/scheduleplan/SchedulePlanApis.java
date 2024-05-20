5740058d16546bd18abff2b19095e7ef1f557d47
3034909308@qq.com <3034909308@qq.com>
2024-05-20 19:24:47 +0800

    新增了api模块下api接口，包括根据排班计划id查询班次计划，具体的班次计划，新增和修改班次计划

--- mes-java/mes-apis/src/main/java/com/zeroone/star/project/j5/scheduleplan/SchedulePlanApis.java
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j5/scheduleplan/SchedulePlanApis.java
@@ -1,5 +1,13 @@
 package com.zeroone.star.project.j5.scheduleplan;
 
+import com.zeroone.star.project.j5.dto.shiftplan.ShiftPlanDto;
+import com.zeroone.star.project.j5.query.shiftplan.ShiftPlanQuery;
+import com.zeroone.star.project.j5.vo.shiftplan.ShiftPlanAddVo;
+import com.zeroone.star.project.j5.vo.shiftplan.ShiftPlanUpdateVo;
+import com.zeroone.star.project.vo.JsonVO;
+
+import java.util.List;
+
 /**
  * <p>
  * 描述：排班计划接口
@@ -10,4 +18,31 @@ package com.zeroone.star.project.j5.scheduleplan;
  * @version 1.0.0
  */
 public interface SchedulePlanApis {
+    /**
+     * 根据排班计划id来查询班次计划列表
+     * @param shiftPlanQuery 排班计划id，具体的排班计划表的id，
+     * @return  班次计划列表
+     */
+    JsonVO<List<ShiftPlanDto>> queryShiftPlanByPlanId(ShiftPlanQuery shiftPlanQuery);
+
+    /**
+     * 根据具体的班次id查找数据
+     * @param shiftId 班次id，如 1
+     * @return 对应id的班次计划
+     */
+    JsonVO<ShiftPlanDto> queryShiftPlan(Integer shiftId);
+
+    /**
+     * 新增班次计划
+     * @param shiftPlanAddVo 新增班次计划数据
+     * @return  新增数据的唯一编号
+     */
+    JsonVO<Integer> addShiftPlan(ShiftPlanAddVo shiftPlanAddVo);
+
+    /**
+     * 更新班次计划
+     * @param shiftPlanUpdateVo 更新班次计划数据
+     * @return  修改的行数
+     */
+    JsonVO<Integer> updateShiftPlan(ShiftPlanUpdateVo shiftPlanUpdateVo);
 }
