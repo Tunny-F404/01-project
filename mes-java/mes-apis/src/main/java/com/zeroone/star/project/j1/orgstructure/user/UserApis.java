d653822f1561c24ba03f0c986bc11004c28573c4
matao <1251281430@qq.com>
2024-05-20 20:49:05 +0800

    在UserApis中编写了获取用户列表接口

--- mes-java/mes-apis/src/main/java/com/zeroone/star/project/j1/orgstructure/user/UserApis.java
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j1/orgstructure/user/UserApis.java
@@ -1,11 +1,24 @@
 package com.zeroone.star.project.j1.orgstructure.user;
 
+import com.zeroone.star.project.j1.orgstructure.dto.user.UserDTO;
+import com.zeroone.star.project.j1.orgstructure.query.user.UserQuery;
+import com.zeroone.star.project.vo.JsonVO;
+
+import java.util.List;
+
 /**
  * <p>
  *     描述：用户管理接口
  * </p>
- * @author twelve
+ * @author Tme
  * @version 1.0.0
  */
-public class UserApis {
+public interface UserApis {
+    /**
+     * 获取用户列表
+     * @param userQuery 查询条件对象
+     * @return 查询结果列表
+     */
+    JsonVO<List<UserDTO>> queryUserList(UserQuery userQuery);
+    
 }
