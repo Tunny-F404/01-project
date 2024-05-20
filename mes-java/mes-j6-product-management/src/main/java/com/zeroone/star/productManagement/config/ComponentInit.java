9e64128c91124b41898fe233478a2c45f1f81ba2
“dataclover” <3123595411@qq.com>
2024-05-20 23:56:28 +0800

    修改code-generator配置文件，并且添加Componentinit扫包

---
+++ mes-java/mes-j6-product-management/src/main/java/com/zeroone/star/productManagement/config/ComponentInit.java
@@ -0,0 +1,13 @@
+package com.zeroone.star.productManagement.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan({
+        "com.zeroone.star.project.components.fastdfs",
+        "com.zeroone.star.project.components.easyexcel"
+})
+public class ComponentInit {
+}
