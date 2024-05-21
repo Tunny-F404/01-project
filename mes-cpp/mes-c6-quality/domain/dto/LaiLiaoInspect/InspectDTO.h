435138396fa111eb38cb81cfd25ecb5c45642688
Dream_memory <2298901711@qq.com>
2024-05-21 20:04:29 +0800

    更新

---
+++ mes-cpp/mes-c6-quality/domain/dto/LaiLiaoInspect/InspectDTO.h
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef _INSPECTDTO_H
+#define _INSPECTDTO_H
+
+#include "../../GlobalInclude.h"
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+/**
+ * 来料检验单数据传输对象
+ */
+class InspectDTO:public oatpp::DTO
+{
+	DTO_INIT(InspectDTO, DTO);
+	//// 隐藏id
+	//API_DTO_FIELD_DEFAULT(UInt64, id, ZH_WORDS_GETTER("inspect.id"));
+	// 来料检验单编号
+	API_DTO_FIELD_DEFAULT(String, dode, ZH_WORDS_GETTER("inspect.code"));
+	// 来料检验单名称
+	API_DTO_FIELD_DEFAULT(String, name, ZH_WORDS_GETTER("inspect.name"));
+	// 供应商简称
+	API_DTO_FIELD_DEFAULT(String, vd_nick, ZH_WORDS_GETTER("inspect.vd_nick"));
+	//供应商批次号
+	API_DTO_FIELD_DEFAULT(String, vd_batch, ZH_WORDS_GETTER("inspect.vd_batch"));
+	//产品物料编码
+	API_DTO_FIELD_DEFAULT(String, item_code, ZH_WORDS_GETTER("inspect.item_code"));
+	//产品物料名称
+	API_DTO_FIELD_DEFAULT(String, item_name, ZH_WORDS_GETTER("inspect.item_name"));
+	//接收数量
+	API_DTO_FIELD_DEFAULT(UInt64, quantity_recived, ZH_WORDS_GETTER("inspect.quantity_recived"));
+	//检测数量
+	API_DTO_FIELD_DEFAULT(UInt64, quantity_check, ZH_WORDS_GETTER("inspect.quantity_check"));
+	//不合格数
+	API_DTO_FIELD_DEFAULT(UInt64, quantity_unqualified, ZH_WORDS_GETTER("inspect.quantity_unqualified"));
+	//检测结果
+	API_DTO_FIELD_DEFAULT(String, check_result, ZH_WORDS_GETTER("inspect.check_result"));
+	//来料日期
+	API_DTO_FIELD_DEFAULT(String, recive_date, ZH_WORDS_GETTER("inspect.recive_date"));
+	//检测日期
+	API_DTO_FIELD_DEFAULT(String, inspect_date, ZH_WORDS_GETTER("inspect.inspect_date"));
+	//检测人员
+	API_DTO_FIELD_DEFAULT(String, inspector, ZH_WORDS_GETTER("inspect.inspector"));
+	//单据状态
+	API_DTO_FIELD_DEFAULT(String, status, ZH_WORDS_GETTER("inspect.status"));
+};
+
+class InspectPageDTO : public PageDTO<InspectDTO::Wrapper>
+{
+	DTO_INIT(InspectPageDTO, PageDTO<InspectDTO::Wrapper>);
+};
+
+#include OATPP_CODEGEN_END(DTO)
+
+#endif // !_INSPECTDTO_H_
