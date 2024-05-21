ed0b132d0098a08db0997e69ce8efb85be0cd426
Axelrod <110959893+Acxlrod@users.noreply.github.com>
2024-05-21 11:28:32 +0800

    增加了生成文档必要的注释

--- mes-java/mes-j3-message/src/main/java/com/zeroone/star/message/controller/MessageInfoController.java
+++ mes-java/mes-j3-message/src/main/java/com/zeroone/star/message/controller/MessageInfoController.java
@@ -1,12 +1,8 @@
 package com.zeroone.star.message.controller;
 
 import com.zeroone.star.project.j3.messageinform.MessageInfoApis;
-<<<<<<< HEAD
-
-=======
 import io.swagger.annotations.Api;
 
 @Api(tags = "消息通知")
->>>>>>> 增加了类上的Api注释
 public class MessageInfoController implements MessageInfoApis {
 }
