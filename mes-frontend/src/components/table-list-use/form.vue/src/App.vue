8c6b5b5191c93e841197d797d15d4826d122d741
f1-W <wxf1998007@163.com>
2024-05-21 21:11:57 +0800

    table-list-use

---
+++ mes-frontend/src/components/table-list-use/form.vue/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <el-button type="primary" @click="openDialog">打开表单</el-button>
+    <CommonDialog ref="commonDialog" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import CommonDialog from './components/CommonDialog.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CommonDialog
+  },
+  setup() {
+    const commonDialog = ref(null);
+
+    const openDialog = () => {
+      commonDialog.value.openDialog();
+    };
+
+    return {
+      commonDialog,
+      openDialog
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  padding: 20px;
+}
+</style>
