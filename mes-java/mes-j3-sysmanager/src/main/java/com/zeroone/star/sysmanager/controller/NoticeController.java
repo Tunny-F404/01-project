94badc84aafb4fb5cedd59d4494adc080d9e2f44
lbw <2149106199@qq.com>
2024-05-21 21:05:28 +0800

    完成了消息模块的模型定义，api，实现controller

--- mes-java/mes-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/NoticeController.java
+++ mes-java/mes-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/NoticeController.java
@@ -1,13 +1,22 @@
 package com.zeroone.star.sysmanager.controller;
 
+import com.zeroone.star.project.j3.dto.NoticeDto;
 import com.zeroone.star.project.j3.sysmanager.NoticeApis;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.hibernate.validator.constraints.pl.REGON;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = "通知公告")
-@Controller
+@RestController
 @RequestMapping("/notice")
 public class NoticeController implements NoticeApis {
+    @PutMapping("/update")
+    @ApiOperation(value = "修改公告")
+    public NoticeDto UpdateNoticeById(Integer id) {
+        return null;
+    }
 }
