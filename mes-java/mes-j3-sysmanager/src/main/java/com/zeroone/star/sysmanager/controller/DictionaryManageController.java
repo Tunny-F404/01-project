18596e856a6bd6ed3b44675ac3bda65f15ff50bf
wanyupeng <2453867667@qq.com>
2024-05-21 16:36:17 +0800

    完成服务启动生成knife4j接口文档

--- mes-java/mes-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/DictionaryManageController.java
+++ mes-java/mes-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/DictionaryManageController.java
@@ -4,7 +4,7 @@ import com.zeroone.star.project.j3.dto.SysDictDataDTO;
 import com.zeroone.star.project.j3.sysmanager.DictionaryManageApis;
 import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import com.zeroone.star.project.vo.JsonVO;
 import io.swagger.annotations.ApiOperation;
 
@@ -15,19 +15,22 @@ public class DictionaryManageController implements DictionaryManageApis {
 
     @Override
     @ApiOperation(value = "新增字典数据接口")
-    public JsonVO<Integer> addDictData(SysDictDataDTO sysDictDataDTO) {
+    @PostMapping("add-dict-data")
+    public JsonVO<Integer> addDictData(@RequestBody SysDictDataDTO sysDictDataDTO) {
         return null;
     }
 
     @Override
     @ApiOperation(value = "修改字典数据接口")
-    public JsonVO<Integer> modifyDictData(SysDictDataDTO sysDictDataDTO) {
+    @PutMapping("modify-dict-data")
+    public JsonVO<Integer> modifyDictData(@RequestBody SysDictDataDTO sysDictDataDTO) {
         return null;
     }
 
-    @ApiOperation(value = "删除字典数据接口")
     @Override
-    public JsonVO<Void> removeDictData(Long[] dictCodes) {
+    @ApiOperation(value = "删除字典数据接口")
+    @DeleteMapping("remove-dict-data/{dictCodes}")
+    public JsonVO<Void> removeDictData(@PathVariable Long[] dictCodes) {
         return null;
     }
 }
