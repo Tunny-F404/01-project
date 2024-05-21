93cc00fcb05546c00f51cc8585ecc9d1fad17dd4
Axelrod <110959893+Acxlrod@users.noreply.github.com>
2024-05-21 12:24:16 +0800

    规范了注释命名

--- mes-java/mes-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/NoticeController.java
+++ mes-java/mes-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/NoticeController.java
@@ -2,7 +2,12 @@ package com.zeroone.star.sysmanager.controller;
 
 import com.zeroone.star.project.j3.sysmanager.NoticeApis;
 import io.swagger.annotations.Api;
+import org.hibernate.validator.constraints.pl.REGON;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Api(tags = "通知公告")
+@Controller
+@RequestMapping("/notice")
 public class NoticeController implements NoticeApis {
 }
