a0d975abb285f59f7ce0fb866fa24f35667038c0
qwert <qwert@qq.com>
2024-05-21 09:28:18 +0800

    更新数据到对应api接口

--- mes-java/mes-apis/src/main/java/com/zeroone/star/project/j5/scheduleplan/SchedulePlanApis.java
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j5/scheduleplan/SchedulePlanApis.java
@@ -8,48 +8,7 @@ import com.zeroone.star.project.vo.JsonVO;
 
 import java.util.List;
 
-/**
- * <p>
- * 描述：排班计划接口
- * </p>
- * <p>版权：&copy;01星球</p>
- * <p>地址：01星球总部</p>
- * @author interstellar
- * @version 1.0.0
- */
+
 public interface SchedulePlanApis {
-    /**
-     * 根据排班计划id来查询班次计划列表
-     * @param shiftPlanQuery 排班计划id，具体的排班计划表的id，
-     * @return  班次计划列表
-     */
-    JsonVO<List<ShiftPlanDto>> queryShiftPlanByPlanId(ShiftPlanQuery shiftPlanQuery);
-
-    /**
-     * 根据具体的班次id查找数据
-     * @param shiftId 班次id，如 1
-     * @return 对应id的班次计划
-     */
-    JsonVO<ShiftPlanDto> queryShiftPlan(Long shiftId);
-
-    /**
-     * 新增班次计划
-     * @param shiftPlanAddDto 新增班次计划数据
-     * @return  新增数据的唯一编号
-     */
-    JsonVO<Long> addShiftPlan(ShiftPlanAddDto shiftPlanAddDto);
-
-    /**
-     * 更新班次计划
-     * @param shiftPlanUpdateDto 更新班次计划数据
-     * @return  修改的行数
-     */
-    JsonVO<Long> updateShiftPlan(ShiftPlanUpdateDto shiftPlanUpdateDto);
-
-    /**
-     * 删除排班计划v1
-     * @param shiftId 排班计划id
-     * @return  修改的行数
-     */
-    JsonVO<Long> deleteShiftPlan(List<Long> shiftId);
+    
 }
