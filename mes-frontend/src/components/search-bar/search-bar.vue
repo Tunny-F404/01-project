cfe76d8647d5fcf375b68dab80462bc5220adc72
f1-阮喵喵 <1219043956@qq.com>
2024-05-22 00:24:13 +0800

    🤔 save-file: 配置类型别名声明；定义通用的表单类型声明变量；

--- mes-frontend/src/components/search-bar/search-bar.vue
+++ mes-frontend/src/components/search-bar/search-bar.vue
@@ -1,26 +1,79 @@
-<script lang="ts" setup>
+<script lang="ts" setup generic="DataType extends Record<string, unknown>">
 import { ref, computed, watch } from 'vue'
 
 import { ElForm, ElInput } from 'element-plus'
+
+type ElInputPropsType = InstanceType<typeof ElInput>['props']
+
+const formType = <const>[
+	'input',
+	'select',
+	'date',
+	'range',
+	'cascader',
+	'radio',
+	'checkbox',
+	'switch',
+	'slider',
+	'rate',
+	'color-picker',
+	'time-picker',
+	'date-picker',
+	'upload',
+	'input-number',
+	'autocomplete',
+	'checkbox-button',
+	'radio-button',
+	'time-select',
+	'time-range-picker',
+	'date-range-picker',
+	'select-tree',
+	'transfer',
+	'form'
+]
+
+type FormType = (typeof formType)[number]
+
+type FormConfigMap = {
+	input: ElInputPropsType
+	[k: string]: any
+}
 
 defineOptions({
 	name: 'SearchBar'
 })
 
-type SearchBarProps = {
+type KeyDataType = keyof DataType
+
+type FormItems<T extends FormType = 'input'> = {
+	type: T
+	label: string
+	prop: KeyDataType
+	config: FormConfigMap[T]
+}
+
+// FormConfigMap['input']
+
+type SearchBarConfig = {
+	formItems: FormItems[]
+}
+
+type SearchBarProps<T = DataType, K = KeyDataType> = {
 	/** 表格配置 */
 	config: {}
 
 	/** 双向绑定的表格数据 */
-	data: Record<string, unknown>
+	data: T
 	// data: string
 }
 
-const props = withDefaults(defineProps<SearchBarProps>(), {
-	config: {}
+const props = withDefaults(defineProps<SearchBarProps<DataType>>(), {
+	config: function () {
+		return {}
+	}
 })
 
-const data = defineModel(<keyof SearchBarProps>'data')
+const data = defineModel<DataType>(<keyof SearchBarProps>'data')
 </script>
 
 <template>
