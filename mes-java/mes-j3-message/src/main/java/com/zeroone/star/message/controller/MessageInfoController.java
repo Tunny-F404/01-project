a6db72e1954cacb1127b206a1e3741cf2fe8ba37
Axelrod <110959893+Acxlrod@users.noreply.github.com>
2024-05-21 09:08:28 +0800

    增加了类上的Api注释

--- mes-java/mes-j3-message/src/main/java/com/zeroone/star/message/controller/MessageInfoController.java
+++ mes-java/mes-j3-message/src/main/java/com/zeroone/star/message/controller/MessageInfoController.java
@@ -1,6 +1,12 @@
 package com.zeroone.star.message.controller;
 
 import com.zeroone.star.project.j3.messageinform.MessageInfoApis;
+<<<<<<< HEAD
 
+=======
+import io.swagger.annotations.Api;
+
+@Api(tags = "消息通知")
+>>>>>>> 增加了类上的Api注释
 public class MessageInfoController implements MessageInfoApis {
 }
--- mes-java/mes-j3-message/src/main/java/com/zeroone/star/message/controller/MessageServiceController.java
+++ mes-java/mes-j3-message/src/main/java/com/zeroone/star/message/controller/MessageServiceController.java
@@ -1,21 +1,8 @@
 package com.zeroone.star.message.controller;
 
-import com.zeroone.star.project.j3.dto.PageDto;
 import com.zeroone.star.project.j3.messageservice.MessageServiceApis;
-import com.zeroone.star.project.j3.query.NewsPageQuery;
-import com.zeroone.star.project.j3.vo.NewsPageVo;
-import com.zeroone.star.project.j3.vo.NewsVo;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Api(tags = "消息服务")
-@Controller
-@RequestMapping("/message-service")
 public class MessageServiceController implements MessageServiceApis {
 }
