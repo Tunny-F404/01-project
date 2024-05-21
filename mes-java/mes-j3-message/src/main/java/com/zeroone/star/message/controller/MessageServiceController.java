695c3f589bb0a9055201003d5d6d20839875fddd
Axelrod <110959893+Acxlrod@users.noreply.github.com>
2024-05-21 11:28:32 +0800

    增加了生成文档必要的注释

--- mes-java/mes-j3-message/src/main/java/com/zeroone/star/message/controller/MessageServiceController.java
+++ mes-java/mes-j3-message/src/main/java/com/zeroone/star/message/controller/MessageServiceController.java
@@ -2,7 +2,11 @@ package com.zeroone.star.message.controller;
 
 import com.zeroone.star.project.j3.messageservice.MessageServiceApis;
 import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Api(tags = "消息服务")
+@Controller
+@RequestMapping("/message-service")
 public class MessageServiceController implements MessageServiceApis {
 }
--- mes-java/mes-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/MessageManageController.java
+++ mes-java/mes-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/MessageManageController.java
@@ -2,7 +2,11 @@ package com.zeroone.star.sysmanager.controller;
 
 import com.zeroone.star.project.j3.sysmanager.MessageManageApis;
 import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Api(tags = "消息管理")
+@Controller
+@RequestMapping("/message-manage")
 public class MessageManageController implements MessageManageApis {
 }
