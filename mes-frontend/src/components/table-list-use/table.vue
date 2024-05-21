6e3bb1cfdd9c0299611c61d5f573eaf2834bcb3b
Harry_Cong <lq087611@163.com>
2024-05-21 19:30:07 +0800

    按照elementplus提供组件文档进行的二次修改表单，在另一个空的vue项目上弄够运行显示，在该框架下还未测试，后续继续学习更新

--- mes-frontend/src/components/table-list-use/table.vue
+++ mes-frontend/src/components/table-list-use/table.vue
@@ -1,94 +1,79 @@
 <template>
-    <div>
-        <el-table :data="filteredData" :default-sort="{ prop: 'date', order: 'ascending' }" style="width: 100%">
-            <el-table-column prop="date" label="日期" sortable width="180">
-            </el-table-column>
-            <el-table-column prop="name" label="姓名" sortable width="180">
-            </el-table-column>
-            <el-table-column prop="address" label="地址">
-            </el-table-column>
-        </el-table>
-        <el-pagination v-show="totalPages > 1" @current-change="handleCurrentChange" :current-page="currentPage"
-            :page-sizes="[10, 20, 30, 50]" :page-size="pageSize" :total="filteredData.length">
-        </el-pagination>
-    </div>
+    <el-button @click="resetDateFilter">reset date filter</el-button>
+    <el-button @click="clearFilter">reset all filters</el-button>
+    <el-table ref="tableRef" row-key="date" :data="tableData" style="width: 100%">
+        <el-table-column prop="date" label="Date" sortable width="180" column-key="date" :filters="[
+            { text: '2016-05-01', value: '2016-05-01' },
+            { text: '2016-05-02', value: '2016-05-02' },
+            { text: '2016-05-03', value: '2016-05-03' },
+            { text: '2016-05-04', value: '2016-05-04' },
+        ]" :filter-method="filterHandler" />
+        <el-table-column prop="name" label="Name" width="180" />
+        <el-table-column prop="address" label="Address" :formatter="formatter" />
+
+        <el-table-column prop="tag" label="Tag" width="100" :filters="[
+            { text: 'Home', value: 'Home' },
+            { text: 'Office', value: 'Office' },
+        ]" :filter-method="filterTag" filter-placement="bottom-end">
+            <template #default="scope">
+                <el-tag :type="scope.row.tag === 'Home' ? '' : 'success'" disable-transitions>{{ scope.row.tag
+                    }}</el-tag>
+            </template>
+        </el-table-column>
+    </el-table>
 </template>
 
-<script>
-import { ElTable, ElTableColumn, ElPagination } from 'element-plus';
-import { ref, reactive, computed } from 'vue';
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { TableColumnCtx, TableInstance } from 'element-plus'
+//原本是const tableRef = ref(null)但是会爆红，还没找到元解决方案
+const tableRef = ref()
 
+const resetDateFilter = () => {
+    tableRef.value.clearFilter(['date'])
+}
 
+const clearFilter = () => {
+    tableRef.value.clearFilter()
+}
 
-export default {
-    components: {
-        ElTable,
-        ElTableColumn,
-        ElPagination
+const formatter = (row, column) => {
+    return row.address
+}
+
+const filterTag = (value, row) => {
+    return row.tag === value
+}
+
+const filterHandler = (value, row, column) => {
+    const property = column.property
+    return row[property] === value
+}
+
+const tableData = [
+    {
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        tag: 'Home',
     },
-    setup() {
-        const tableData = ref([
-            { date: '2024-05-01', name: '张三', address: '北京市朝阳区' },
-            { date: '2024-05-02', name: '李四', address: '上海市浦东新区' },
-            { date: '2024-05-03', name: '王五', address: '广州市天河区' },
-            // 更多数据...
-        ]);
-
-        const filter = reactive({
-            date: '',
-            name: '',
-            address: ''
-        });
-
-        const sort = reactive({
-            prop: 'date',
-            order: 'ascending'
-        });
-
-        const currentPage = ref(1);
-        const pageSize = ref(10);
-
-        const handleCurrentChange = (val) => {
-            currentPage.value = val;
-        };
-
-
-        const filteredData = computed(() => {
-            let result = tableData.value;
-
-            // 过滤
-            if (filter.date) {
-                result = result.filter(item => item.date.includes(filter.date));
-            }
-            if (filter.name) {
-                result = result.filter(item => item.name.includes(filter.name));
-            }
-            if (filter.address) {
-                result = result.filter(item => item.address.includes(filter.address));
-            }
-
-            // 排序
-            result.sort((a, b) => {
-                const order = sort.order === 'ascending' ? 1 : -1;
-                return order * (a[sort.prop] > b[sort.prop] ? 1 : -1);
-            });
-
-            // 分页
-            const startIndex = (currentPage.value - 1) * pageSize.value;
-            const endIndex = startIndex + pageSize.value;
-            return result.slice(startIndex, endIndex);
-        });
-
-
-        const totalPages = computed(() => Math.ceil(filteredData.value.length / pageSize.value));
-
-        return {
-            filteredData,
-            currentPage,
-            pageSize,
-            totalPages,
-            handleCurrentChange
-        };
-    }
-};
+    {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        tag: 'Office',
+    },
+    {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        tag: 'Home',
+    },
+    {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        tag: 'Office',
+    },
+]
 </script>
