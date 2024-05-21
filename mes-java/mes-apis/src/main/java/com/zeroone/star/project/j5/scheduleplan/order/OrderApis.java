f975341b75bb137a034dc5cf6fa3777d22f7d4ab
qwert <qwert@qq.com>
2024-05-21 21:37:42 +0800

    删除了详情查询

--- mes-java/mes-apis/src/main/java/com/zeroone/star/project/j5/scheduleplan/order/OrderApis.java
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j5/scheduleplan/order/OrderApis.java
@@ -27,13 +27,6 @@ public interface OrderApis {
     JsonVO<PageDTO<ShiftPlanDTO>> queryShiftPlanByPlanId(ShiftPlanQuery shiftPlanQuery);
 
     /**
-     * 根据具体的班次id查找数据
-     * @param shiftId 班次id，如 1
-     * @return 对应id的班次计划
-     */
-    //JsonVO<ShiftPlanDTO> queryShiftPlan(Long shiftId);
-
-    /**
      * 新增班次计划
      * @param shiftPlanAddDto 新增班次计划数据
      * @return  新增数据的唯一编号
