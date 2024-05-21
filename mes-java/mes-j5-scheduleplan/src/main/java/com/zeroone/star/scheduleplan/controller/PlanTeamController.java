72e132d954e538ac26b9d709b6e7d05c72338402
linglan111 <15192050246@163.com>
2024-05-21 20:35:45 +0800

    add: 初步创建编写关联班组(plan_team)相关: controller文件

--- mes-java/mes-j5-scheduleplan/src/main/java/com/zeroone/star/scheduleplan/controller/PlanTeamController.java
+++ mes-java/mes-j5-scheduleplan/src/main/java/com/zeroone/star/scheduleplan/controller/PlanTeamController.java
@@ -2,11 +2,13 @@ package com.zeroone.star.scheduleplan.controller;
 
 import com.zeroone.star.project.dto.PageDTO;
 import com.zeroone.star.project.dto.sample.SampleDTO;
+import com.zeroone.star.project.j5.dto.planteam.AddPlanTeamDTO;
 import com.zeroone.star.project.j5.dto.planteam.PlanTeamDTO;
 import com.zeroone.star.project.j5.query.planteam.PlanTeamQuery;
 import com.zeroone.star.project.j5.scheduleplan.TeamPlanApis;
 import com.zeroone.star.project.query.sample.SampleQuery;
 import com.zeroone.star.project.vo.JsonVO;
+import com.zeroone.star.project.vo.ResultStatus;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
@@ -26,10 +28,23 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "关联班组")
 public class PlanTeamController implements TeamPlanApis {
 
-        @GetMapping("/queryAll")
+        @GetMapping("/list")
         @ApiOperation("查询指定条件的关联班组")
         @Override
-        public JsonVO<PageDTO<PlanTeamDTO>> queryAll(PlanTeamQuery condition) {
+        public JsonVO<PageDTO<PlanTeamDTO>> queryAllPlanTeam(PlanTeamQuery condition) {
+                return null;
+        }
+        @GetMapping("/add")
+        @ApiOperation("添加关联班组")
+        @Override
+        public JsonVO<ResultStatus> addPlanTeam(AddPlanTeamDTO addPlanTeamDTO) {
+                return null;
+        }
+
+        @GetMapping("/delete")
+        @ApiOperation("删除关联班组")
+        @Override
+        public JsonVO<ResultStatus> deletePlanTeam(Long id) {
                 return null;
         }
 }
