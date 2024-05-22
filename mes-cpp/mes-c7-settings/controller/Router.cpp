c4b09afe2d64b0028d5ea51a9902cff5a18b5b4d
MoYi <2296396559@qq.com>
2024-05-22 21:05:18 +0800

    合并分支 shen yintong

--- mes-cpp/mes-c7-settings/controller/Router.cpp
+++ mes-cpp/mes-c7-settings/controller/Router.cpp
@@ -19,6 +19,7 @@
 #include "stdafx.h"
 #include "Router.h"
 #include "ApiHelper.h"
+#include "storage_area/GetAreaDetailsController.h"
 
 #include "add-del-mod/AddDelModController.h"
 
@@ -53,6 +54,7 @@ void Router::initRouter()
 	//#TIP :系统扩展路由定义，写在这个后面
 	
 	ROUTER_SIMPLE_BIND(AddDelModController);
+	ROUTER_SIMPLE_BIND(GetAreaDetailsController);
 }
 
 #ifdef HTTP_SERVER_DEMO
