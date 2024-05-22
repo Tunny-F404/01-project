9dd3e28a358aa00d87ff653e629cb9f6d4224370
Youm <youmiao015@gmail.com>
2024-05-22 23:33:55 +0800

    增加系统通知相关领域模型和接口

--- mes-java/mes-apis/src/main/java/com/zeroone/star/project/j3/sysmanager/NoticeApis.java
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j3/sysmanager/NoticeApis.java
@@ -1,8 +1,19 @@
 package com.zeroone.star.project.j3.sysmanager;
 
+import com.zeroone.star.project.dto.login.LoginDTO;
+import com.zeroone.star.project.j3.dto.LogoutDTO;
+import com.zeroone.star.project.j3.dto.SystemNotificationDTO;
+import com.zeroone.star.project.vo.ResultStatus;
+import com.zeroone.star.project.vo.login.LoginVO;
+
 /**
  * 系统管理-通知公告相关接口
  */
 
 public interface NoticeApis {
+    LoginVO clientLogin(LoginDTO loginDTO);
+
+    ResultStatus clientLogout(LogoutDTO logoutDTO);
+
+    ResultStatus sendNotice(SystemNotificationDTO systemNotificationDTO);
 }
--- mes-java/mes-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/NoticeController.java
+++ mes-java/mes-j3-sysmanager/src/main/java/com/zeroone/star/sysmanager/controller/NoticeController.java
@@ -1,6 +1,38 @@
 package com.zeroone.star.sysmanager.controller;
 
+import com.zeroone.star.project.dto.login.LoginDTO;
+import com.zeroone.star.project.j3.dto.LogoutDTO;
+import com.zeroone.star.project.j3.dto.SystemNotificationDTO;
 import com.zeroone.star.project.j3.sysmanager.NoticeApis;
+import com.zeroone.star.project.vo.ResultStatus;
+import com.zeroone.star.project.vo.login.LoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "系统通知接口")
+@RestController
+@RequestMapping("/api/system-notice")
 public class NoticeController implements NoticeApis {
+    @ApiOperation("客户端登录")
+    @PostMapping("/login")
+    public LoginVO clientLogin(@ApiParam("登录信息") @RequestBody LoginDTO loginDTO) {
+        return null;
+    }
+
+    @ApiOperation("客户端退出")
+    @PostMapping("/logout")
+    public ResultStatus clientLogout(@ApiParam("退出信息") @RequestBody LogoutDTO logoutDTO) {
+        return null;
+    }
+
+    @ApiOperation("发送系统通知")
+    @PostMapping("/send-notice")
+    public ResultStatus sendNotice(@ApiParam("系统通知消息") @RequestBody SystemNotificationDTO systemNotificationDTO) {
+        return null;
+    }
 }
