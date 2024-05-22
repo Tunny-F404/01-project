61f5f112f570d5b2c8fe5c32cb7030193e3ceb35
zebul <1733012467@qq.com>
2024-05-22 15:36:51 +0800

    用于调试实现仓储管理-生产领料-添加单据，修改单据

--- mes-cpp/mes-c6-storage-product/main.cpp
+++ mes-cpp/mes-c6-storage-product/main.cpp
@@ -45,7 +45,7 @@ bool getStartArg(int argc, char* argv[]) {
 	std::string serverPort = "8090";
 	// 数据库连接信息
 	std::string dbUsername = "root";
-	std::string dbPassword = "123456";
+	std::string dbPassword = "122500";
 	std::string dbName = "test";
 	std::string dbHost = "192.168.220.128";
 	int dbPort = 3306;
