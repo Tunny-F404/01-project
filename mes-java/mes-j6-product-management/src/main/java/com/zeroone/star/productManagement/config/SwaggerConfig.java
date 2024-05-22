6592d0d601c40cae1a750996db909d327dabba46
zebraG <1033416242@qq.com>
2024-05-22 23:46:28 +0800

    修复 Bomdto内 某些字段数据类型不匹配，修改nacos配置

--- mes-java/mes-j6-product-management/src/main/java/com/zeroone/star/productManagement/config/SwaggerConfig.java
+++ mes-java/mes-j6-product-management/src/main/java/com/zeroone/star/productManagement/config/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package com.zeroone.star.productManagement.config;
 
 import com.zeroone.star.project.config.swagger.SwaggerCore;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+@Configuration
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
     Docket Api() {
         return SwaggerCore.defaultDocketBuilder("物料管理模块",
