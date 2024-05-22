7fe5e6746601d69fe8c0764cd1936939bd1b2159
Liao Yan <liaoxueyan030106@gmail.com>
2024-05-22 23:49:25 +0800

    完成了4个controller，还未具体实现

--- mes-java/mes-j5-scheduleplan/src/main/java/com/zeroone/star/scheduleplan/controller/SchedulePlanController.java
+++ mes-java/mes-j5-scheduleplan/src/main/java/com/zeroone/star/scheduleplan/controller/SchedulePlanController.java
@@ -7,11 +7,15 @@ import com.zeroone.star.project.j5.query.scheduleplan.PlanPageQuery;
 import com.zeroone.star.project.j5.scheduleplan.SchedulePlanApis;
 import com.zeroone.star.project.j5.vo.scheduleplan.PlanListVO;
 import com.zeroone.star.project.j5.vo.scheduleplan.PlanVO;
+import com.zeroone.star.project.query.PageQuery;
 import com.zeroone.star.project.vo.JsonVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Min;
 
 /**
  * <p>
@@ -22,24 +26,28 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("sch-manage/sch-plan")
-@Api(tags = "班组接口")
+@Api(tags = "排班计划接口")
 public class SchedulePlanController implements SchedulePlanApis {
-    @Override
-    public JsonVO<PageDTO<PlanListVO>> queryPlanList(PlanPageQuery condition) {
+    @GetMapping("/query-all")
+    @ApiOperation("分页条件查询排班计划列表")
+    public JsonVO<PageDTO<PlanListVO>> queryPlanList(@Validated PlanPageQuery condition) {
         return null;
     }
 
-    @Override
-    public JsonVO<PlanVO> queryPlanDetails(Long planId) {
+    @GetMapping("query-by-id")
+    @ApiOperation("获取排班计划详情")
+    public JsonVO<PlanVO> queryPlanDetails(@Min(value = 1,message = "planId最小值为1") @RequestParam Long planId) {
         return null;
     }
 
-    @Override
-    public JsonVO<Integer> addPlan(PlanDTO planDTO) {
+    @ApiOperation("添加排班计划")
+    @PostMapping("add-plan")
+    public JsonVO<Integer> addPlan(PlanDTO condition) {
         return null;
     }
 
-    @Override
+    @ApiOperation("修改排班计划")
+    @PutMapping("modify-team")
     public JsonVO<Integer> modifyTeam(PlanDTO planDTO) {
         return null;
     }
