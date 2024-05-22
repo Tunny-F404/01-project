ce2a8f591858ceb86b501d2094b8896900c4c7a9
MoYi <2296396559@qq.com>
2024-05-22 19:47:05 +0800

    注释掉GetAreaDetailsController.cpp的实现逻辑

--- mes-cpp/mes-c7-settings/controller/storage_area/GetAreaDetailsController.cpp
+++ mes-cpp/mes-c7-settings/controller/storage_area/GetAreaDetailsController.cpp
@@ -2,6 +2,7 @@
 #include "GetAreaDetailsController.h"
 
 GetAreaDetailsPageJsonVO::Wrapper GetAreaDetailsController::executeQueryAll(const GetAreaDetailsQuery::Wrapper& userQuery){
-	auto jvo = GetAreaDetailsJsonVO::createShared();
-	return jvo;
+	// auto jvo = GetAreaDetailsJsonVO::createShared();
+	// return jvo;
+	return {};
 }
