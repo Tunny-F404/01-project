5221cc1ae020354849fb0be513289ee4e7c88e26
junhan-zhao <junhan_zhao@163.com>
2024-05-22 21:17:52 +0800

    节假日设置 controller swagger文档3

--- mes-java/mes-j5-holidaysettings/src/main/java/com/zeroone/star/holidaysettings/controller/HoildaySettingController.java
+++ mes-java/mes-j5-holidaysettings/src/main/java/com/zeroone/star/holidaysettings/controller/HoildaySettingController.java
@@ -5,9 +5,12 @@ import com.zeroone.star.project.j5.holidaysettings.HolidaySettingsApis;
 import com.zeroone.star.project.j5.query.holidaysetting.SettingHolidayQuery;
 import com.zeroone.star.project.vo.JsonVO;
 import com.zeroone.star.project.vo.ResultStatus;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @description:
@@ -17,6 +20,9 @@ import org.springframework.web.bind.annotation.PostMapping;
  * @create: 2024-05-22 21:15
  * @Version 1.0
  **/
+@RestController
+@RequestMapping("/cal/calholiday")
+@Api(tags = "节假日设置")
 public class HoildaySettingController implements HolidaySettingsApis {
     @GetMapping("/list")
     @ApiOperation("获取节假日配置")
