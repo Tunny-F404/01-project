b28c54cbd90724077ce2afce4a31b07ba52f951e
jyh <568000273@qq.com>
2024-05-22 13:00:38 +0800

    MP生成代码

--- mes-java/mes-j2-sysmanagement/src/main/java/com/zeroone/star/sysmanagement/SysManagementApplication.java
+++ mes-java/mes-j2-sysmanagement/src/main/java/com/zeroone/star/sysmanagement/SysManagementApplication.java
@@ -1,5 +1,6 @@
 package com.zeroone.star.sysmanagement;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.zeroone.star.sysmanagement.mapper")
 public class SysManagementApplication {
 
     public static void main(String[] args) {
--- mes-java/mes-j2-sysmanagement/src/test/java/com/zeroone/star/sysmanagement/Test.java
+++ mes-java/mes-j2-sysmanagement/src/test/java/com/zeroone/star/sysmanagement/Test.java
@@ -1,8 +1,12 @@
 package com.zeroone.star.sysmanagement;
 
+import com.zeroone.star.sysmanagement.entity.ParameterDO;
+import com.zeroone.star.sysmanagement.mapper.ParameterMapper;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 /**
  * 描述： TODO
@@ -15,8 +19,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Test {
 
+    @Resource
+    ParameterMapper mapper;
+
     @org.junit.Test
     public void test01(){
-
+        ParameterDO parameterDO = mapper.selectById(1);
+        System.out.println(parameterDO);
     }
 }
