3c750fc9956b65930d0ac844da22d7d203e37336
wanyupeng <2453867667@qq.com>
2024-05-23 15:11:57 +0800

    修改SysDictDataDTO

--- mes-java/mes-domain/src/main/java/com/zeroone/star/project/j3/dto/SysDictDataDTO.java
+++ mes-java/mes-domain/src/main/java/com/zeroone/star/project/j3/dto/SysDictDataDTO.java
@@ -1,20 +1,18 @@
 package com.zeroone.star.project.j3.dto;
 
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.util.Date;
 
 @Data
 @ApiModel(value = "字典数据传输对象")
 public class SysDictDataDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "字典编码")
+    @ApiModelProperty(value = "字典编号")
     private Long dictCode;
 
     @ApiModelProperty(value = "字典排序")
@@ -32,28 +30,11 @@ public class SysDictDataDTO implements Serializable {
     @ApiModelProperty(value = "样式属性")
     private String cssClass;
 
-    @ApiModelProperty(value = "表格字典样式")
+    @ApiModelProperty(value = "表格回显样式")
     private String listClass;
-
-    @ApiModelProperty(value = "是否默认（Y是 N否）")
-    private String isDefault;
 
     @ApiModelProperty(value = "状态（0正常 1停用）")
     private String status;
-
-    @ApiModelProperty(value = "创建人")
-    private String createBy;
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @ApiModelProperty(value = "创建时间")
-    private Date createTime;
-
-    @ApiModelProperty(value = "创建人")
-    private String updateBy;
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @ApiModelProperty(value = "更新时间")
-    private Date updateTime;
 
     @ApiModelProperty(value = "备注")
     private String remark;
