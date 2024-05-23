4a850661e0d9bc046e28a46e581df157f278dbb5
Leyeofi <1361607543@qq.com>
2024-05-23 20:45:33 +0800

    领域模型、接口定义和Nacos配置修改

--- mes-java/mes-apis/src/main/java/com/zeroone/star/project/j3/messageservice/MessageServiceApis.java
+++ mes-java/mes-apis/src/main/java/com/zeroone/star/project/j3/messageservice/MessageServiceApis.java
@@ -1,8 +1,21 @@
 package com.zeroone.star.project.j3.messageservice;
 
+import com.zeroone.star.project.j3.dto.MessageDTO;
+import com.zeroone.star.project.j3.query.MessageQuery;
+import com.zeroone.star.project.vo.JsonVO;
+
+import java.util.List;
+
 /**
  * 消息服务相关接口
  */
 
 public interface MessageServiceApis {
+
+    JsonVO<List<Long>> removeMessages(List<Long> ids);
+
+    JsonVO<List<MessageDTO>> queryMessage(MessageQuery query);
+
+    JsonVO<List<Long>> modifyMessageStatus(Long id);
+
 }
--- mes-java/mes-j3-message/src/main/java/com/zeroone/star/message/controller/MessageServiceController.java
+++ mes-java/mes-j3-message/src/main/java/com/zeroone/star/message/controller/MessageServiceController.java
@@ -1,6 +1,43 @@
 package com.zeroone.star.message.controller;
 
+import com.zeroone.star.project.j3.dto.MessageDTO;
 import com.zeroone.star.project.j3.messageservice.MessageServiceApis;
+import com.zeroone.star.project.j3.query.MessageQuery;
+import com.zeroone.star.project.vo.JsonVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Api("消息服务")
+@RestController
+@Validated
+@RequestMapping("/message")
 public class MessageServiceController implements MessageServiceApis {
+    @Override
+    @PostMapping("/delete-messages")
+    @ApiOperation("删除消息")
+    public JsonVO<List<Long>> removeMessages(List<Long> ids) {
+        return null;
+    }
+
+    @Override
+    @GetMapping("/query-messages")
+    @ApiOperation(("查询消息"))
+    public JsonVO<List<MessageDTO>> queryMessage(MessageQuery query) {
+        return null;
+    }
+
+    @Override
+    @PostMapping("/modify-message")
+    @ApiOperation("修改消息状态")
+    public JsonVO<List<Long>> modifyMessageStatus(Long id) {
+        return null;
+    }
 }
