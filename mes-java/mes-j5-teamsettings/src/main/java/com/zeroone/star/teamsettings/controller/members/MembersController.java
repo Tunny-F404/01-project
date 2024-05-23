8970cd0d20bb79d3043e75d9dd78e0dad2c49cd5
j5-liang <2455210119@qq.com>
2024-05-23 19:02:23 +0800

    MembersController 修改

--- mes-java/mes-j5-teamsettings/src/main/java/com/zeroone/star/teamsettings/controller/members/MembersController.java
+++ mes-java/mes-j5-teamsettings/src/main/java/com/zeroone/star/teamsettings/controller/members/MembersController.java
@@ -6,7 +6,10 @@ import com.zeroone.star.project.j5.query.teamsettings.MemberQuery;
 import com.zeroone.star.project.j5.teamsettings.members.MembersApis;
 import com.zeroone.star.project.vo.JsonVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,21 +29,26 @@ import java.util.List;
 @Api(tags = "项目组成员接口")
 @Validated
 public class MembersController implements MembersApis {
+    @ApiOperation(value = "获取班组成员列表（条件 + 分页）")
+    @GetMapping("query-all")
     @Override
     public JsonVO<PageDTO<MemberDTO>> queryMember(MemberQuery condition) {
         return null;
     }
-
+    @ApiOperation(value = "添加班组成员")
+    @PostMapping("add-member")
     @Override
     public JsonVO<Void> addMembers(List<MemberDTO> memberDTOList) {
         return null;
     }
-
+    @ApiOperation(value = "删除班组成员")
+    @PostMapping("delete-member")
     @Override
     public JsonVO<Void> deleteMembers(List<Integer> memberIds) {
         return null;
     }
-
+    @ApiOperation(value = "导出班组成员")
+    @GetMapping("export-member")
     @Override
     public JsonVO<byte[]> exportMembers(MemberQuery condition) {
         return null;
