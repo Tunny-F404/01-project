072a45762f8f13ef05f24f37480ba38bc2e2c4f8
fengdongnan <2326062556@qq.com>
2024-05-23 23:06:09 +0800

    j5-排班计划04-comtroller4--sailing

--- mes-java/mes-domain/src/main/java/com/zeroone/star/project/j5/dto/scheduleplan/SchPlanDTO.java
+++ mes-java/mes-domain/src/main/java/com/zeroone/star/project/j5/dto/scheduleplan/SchPlanDTO.java
@@ -13,25 +13,23 @@ import java.time.LocalDate;
 public class SchPlanDTO {
     @ApiModelProperty(value = "计划id")
     private int planId;
-    @ApiModelProperty(value = "计划编号")
+    @ApiModelProperty(value = "计划编号",example = "PLAN1111")
     private int planCode;
-    @ApiModelProperty(value = "计划名称")
+    @ApiModelProperty(value = "计划名称",example = "工厂翻修1")
     private String planName;
-    @ApiModelProperty(value = "班组类型")
+    @ApiModelProperty(value = "班组类型",example = "注塑")
     private String calendarType;
-    @ApiModelProperty(value = "开始日期")
+    @ApiModelProperty(value = "开始日期",example = "2024-5-02")
     private LocalDate startDate;
-    @ApiModelProperty(value = "结束日期")
+    @ApiModelProperty(value = "结束日期",example = "2025-5-02")
     private LocalDate endDate;
-    @ApiModelProperty(value = "轮班方式")
+    @ApiModelProperty(value = "轮班方式",example = "白班")
     private String shiftType;
-    @ApiModelProperty(value = "倒班方式")
+    @ApiModelProperty(value = "倒班方式",example = "按月")
     private String shiftMethod;
-    @ApiModelProperty(value = "数")
-    private int shiftCount;
-    @ApiModelProperty(value = "计划状态")
+    @ApiModelProperty(value = "计划状态",example = "确认")
     private String status;
-    @ApiModelProperty(value = "注释")
+    @ApiModelProperty(value = "注释",example = "一段备注")
     private String remark;
 
 }
