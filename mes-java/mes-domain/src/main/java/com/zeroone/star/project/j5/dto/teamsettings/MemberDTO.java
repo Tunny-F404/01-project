756ca6e552feca7b872d80a6f1a3bded8aff2531
j5-liang <2455210119@qq.com>
2024-05-24 18:26:08 +0800

    MemberDTO修改参数

--- mes-java/mes-domain/src/main/java/com/zeroone/star/project/j5/dto/teamsettings/MemberDTO.java
+++ mes-java/mes-domain/src/main/java/com/zeroone/star/project/j5/dto/teamsettings/MemberDTO.java
@@ -16,8 +16,8 @@ public class MemberDTO {
     private Object params;
     @ApiModelProperty(value = "用户编号", example = "1")
     private Integer userId;
-    @ApiModelProperty(value = "部门编号", example = "1")
-    private Integer deptId;
+    @ApiModelProperty(value = "部门名称", example = "1")
+    private Integer deptName;
     @ApiModelProperty(value = "用户名称", example = "1")
     private String userName;
     @ApiModelProperty(value = "用户昵称", example = "1")
@@ -26,8 +26,6 @@ public class MemberDTO {
     private String phonenumber;
     @ApiModelProperty(value = "状态", example = "0")//0表示正常 1表示停用
     private String status;
-    @ApiModelProperty(value = "删除标志", example = "1")//0表示未删除，1表示已删除
-    private String delFlag;
     @ApiModelProperty(value = "创建时间", example = "2022-08-24 21：54：31")
     private String createTime;
 }
