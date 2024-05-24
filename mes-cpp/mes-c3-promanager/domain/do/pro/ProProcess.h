1007ce12b581c1bdfa358fcb7c877a0b0d3235d2
bao <2573243608@qq.com>
2024-05-24 21:26:26 +0800

    新增生成工序表do

---
+++ mes-cpp/mes-c3-promanager/domain/do/pro/ProProcess.h
@@ -0,0 +1,56 @@
+#pragma once
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: awei
+ @Date: 2022/10/25 11:51:41
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#ifndef _PRO_PROCESS_
+#define _PRO_PROCESS_
+
+#include "../../Macros.h"
+#include <string>
+#include <list>
+using namespace std;
+//  生产工序表
+
+class ProProcessDO
+{
+	//生产工序ID
+	CC_SYNTHESIZE(string, processid, ProcessId);
+	//生产工序编号
+	CC_SYNTHESIZE(string, processcode, ProcessCode);
+	//生产工序名称
+	CC_SYNTHESIZE(string, processname, ProcessName);
+	//生产工序说明
+	CC_SYNTHESIZE(string, attention, Attention);
+	//是否启用
+	CC_SYNTHESIZE(string, enableflag, EnableFlag);
+	//备注
+	CC_SYNTHESIZE(string, remark, Remark);
+
+public:
+	ProProcessDO() {
+		processid = "";
+		processcode = "";
+		processname = "";
+		attention = "";
+		enableflag = "";
+		remark = "";
+	}
+
+}
+
+#endif // !_PRO_PROCESS_
