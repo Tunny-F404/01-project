f0214726ca2e32fe71ce3e1b6e14f3db23e85adc
wmding <2386585789@qq.com>
2024-05-25 22:36:59 +0800

    fix

--- mes-java/mes-domain/src/main/java/com/zeroone/star/project/j2/sysmanagement/query/menu/MenuConditionQuery.java
+++ mes-java/mes-domain/src/main/java/com/zeroone/star/project/j2/sysmanagement/query/menu/MenuConditionQuery.java
@@ -12,9 +12,9 @@ import lombok.Data;
 @ApiOperation("菜单查询条件")
 public class MenuConditionQuery {
 
-    @ApiModelProperty(value = "菜单名称",example = "基础数据")
+    @ApiModelProperty(value = "菜单名称",example = "基础数据", notes = "支持模糊查询")
     private String menuName;
 
-    @ApiModelProperty(value = "状态",example = "0")
-    private String status;
+    @ApiModelProperty(value = "状态",example = "0", notes = "0表示停用, 1表示正常")
+    private Integer status;
 }
