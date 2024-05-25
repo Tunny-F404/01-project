246e3806b4a66de0626562e02eb7d47cc7866fe0
Harry_Cong <lq087611@163.com>
2024-05-25 19:57:08 +0800

    二次修改，动态删除，分页，过滤与搜索还未测试

--- mes-frontend/src/components/table-list-use/table.vue
+++ mes-frontend/src/components/table-list-use/table.vue
@@ -1,47 +1,98 @@
 <template>
-    <div class="cl-PaginationTable">
-        <el-table :data="tableData" height="320">
-            <el-table-column v-for="col of tableColumns" :key="col.data" :prop="col.data" :label="col.label" />
+    <div>
+        <el-input placeholder="请输入内容" v-model="search" clearable @clear="handleClear"></el-input>
+        <el-table :data="filteredData" style="width: 100%">
+            <el-table-column prop="date" label="日期" width="180"></el-table-column>
+            <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+            <el-table-column prop="address" label="地址"></el-table-column>
+            <el-table-column label="操作">
+                <template #default="scope">
+                    <el-button type="primary" size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                    <el-button type="danger" size="small" @click="handleDelete(scope.$index)">删除</el-button>
+                </template>
+            </el-table-column>
         </el-table>
+        <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage"
+            :page-sizes="[5, 10, 15, 20]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper"
+            :total="filteredData.length"></el-pagination>
     </div>
 </template>
 
-<script setup>
-import { ElTable, ElTableColumn } from "element-plus";
-import { getPeopleInfo } from "@/api/people.js";
-import { onMounted, ref } from "vue";
+<script>
+import { ref, computed } from 'vue'
 
-// t- 初始化表格数据
-const tableData = ref([]);
-const tableColumns = [
-    {
-        label: "人员",
-        data: "name",
-    },
-    {
-        label: "地址",
-        data: "address",
-    },
-    {
-        label: "签到时间",
-        data: "time",
-    },
-];
-async function getTableData() {
-    const res = await getPeopleInfo();
-    tableData.value = res.data;
+export default {
+    setup() {
+        const tableData = ref([
+            {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            },
+            {
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1517 弄'
+            },
+            {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1519 弄'
+            },
+            {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1516 弄'
+            }
+        ])
+        const search = ref('')
+        const currentPage = ref(1)
+        const pageSize = ref(5)
+
+        const filteredData = computed(() => {
+            let result = tableData.value
+            if (search.value) {
+                result = result.filter(row => row.name.includes(search.value) || row.address.includes(search.value))
+            }
+            const start = (currentPage.value - 1) * pageSize.value
+            const end = start + pageSize.value
+            return result.slice(start, end)
+        })
+
+        const handleEdit = (index, row) => {
+            // 在这里添加你的编辑逻辑
+            console.log('编辑', index, row)
+        }
+
+        const handleDelete = (index) => {
+            const start = (currentPage.value - 1) * pageSize.value
+            tableData.value.splice(start + index, 1)
+        }
+
+        const handleSizeChange = val => {
+            pageSize.value = val
+        }
+
+        const handleCurrentChange = val => {
+            currentPage.value = val
+        }
+
+        const handleClear = () => {
+            search.value = ''
+        }
+
+        return {
+            tableData,
+            search,
+            filteredData,
+            currentPage,
+            pageSize,
+            handleEdit,
+            handleDelete,
+            handleSizeChange,
+            handleCurrentChange,
+            handleClear
+        }
+    }
 }
-onMounted(async () => {
-    getTableData();
-});
 </script>
-
-<style lang="less" scoped>
-.cl-PaginationTable {
-    width: 500px;
-    height: 300px;
-    border: 3px solid;
-    margin: 0 auto;
-    padding-bottom: 40px;
-}
-</style>
