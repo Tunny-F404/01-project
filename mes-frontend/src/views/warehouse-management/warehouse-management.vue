4aa3fa4d2709b6bbb8f3fd3513cdc75a3be36f1a
f1-HarryCong <lq087611@163.com>
2024-05-28 22:31:41 +0800

    找到具名插槽错误，表格组件table-true出现的组件问题，联系对应人对应修改

--- mes-frontend/src/views/warehouse-management/warehouse-management.vue
+++ mes-frontend/src/views/warehouse-management/warehouse-management.vue
@@ -13,33 +13,19 @@
             </el-form-item>
 
             <el-form-item>
-                <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
-                <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+                <el-button type="primary" icon="el-icon-search" size="small" @click="handleQuery">搜索</el-button>
+                <el-button icon="el-icon-refresh" size="small" @click="resetQuery">重置</el-button>
             </el-form-item>
 
         </el-form>
 
-        <el-row :gutter="10" class="mb8">
-            <el-col :span="1.5">
-                <el-button type="primary" plain icon="el-icon-plus" size="mini" @click="handleAdd"
-                    v-hasPermi="['mes:wm:warehouse:add']">新增</el-button>
-            </el-col>
-            <el-col :span="1.5">
-                <el-button type="success" plain icon="el-icon-edit" size="mini" :disabled="single" @click="handleUpdate"
-                    v-hasPermi="['mes:wm:warehouse:edit']">修改</el-button>
-            </el-col>
-            <el-col :span="1.5">
-                <el-button type="danger" plain icon="el-icon-delete" size="mini" :disabled="multiple"
-                    @click="handleDelete" v-hasPermi="['mes:wm:warehouse:remove']">删除</el-button>
-            </el-col>
-            <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
-        </el-row>
+
+        <TableTrue #extra>
+<h1>111231323144</h1>
+        </TableTrue>
 
 
 
-          <TableTrue />
-   
-  
     </div>
 
 </template>
@@ -48,7 +34,7 @@ import { ElButton } from "element-plus"
 
 import TableText from '@/components/table-list-use/table-text.vue';
 import TableTrue from '@/components/table-list-use/table-true.vue';
-import PopUp from '@/components/table-list-use/table-components/pop-up.vue';
+
 
 const components = { TableText, TableTrue };
 
@@ -58,7 +44,7 @@ const components = { TableText, TableTrue };
 <style scoped>
 .app-container{
     background-color: aliceblue;
-    width: 800px;
+    width: auto;
     height: 500px;
 }
 .body{
