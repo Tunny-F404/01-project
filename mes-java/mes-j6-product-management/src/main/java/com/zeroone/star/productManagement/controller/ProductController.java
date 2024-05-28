d5f18174091320aa25f80ce5d99be6fecac174ec
anonymity <sbl1213845678@qq.com>
2024-05-28 22:12:12 +0800

    物料管理任务2

--- mes-java/mes-j6-product-management/src/main/java/com/zeroone/star/productManagement/controller/ProductController.java
+++ mes-java/mes-j6-product-management/src/main/java/com/zeroone/star/productManagement/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.zeroone.star.productManagement.controller;
 
+import com.zeroone.star.productManagement.service.IProductService;
 import com.zeroone.star.project.dto.PageDTO;
 import com.zeroone.star.project.dto.ProductDTO;
 import com.zeroone.star.project.dto.ProductListDTO;
@@ -8,45 +9,65 @@ import com.zeroone.star.project.query.ProductQuery;
 import com.zeroone.star.project.vo.JsonVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product-management/product")
 @Api(tags = "产品物料相关接口")
 public class ProductController implements ProductManageApis {
 
+
+    @Autowired
+    private IProductService productService;
+
     @GetMapping("/list")
-    @ApiOperation(value = "查询关联供应商列表")
+    @ApiOperation(value = "分页查询产品（可以添加条件")
     @Override
     public JsonVO<PageDTO<ProductDTO>> queryAll(ProductQuery query) {
-        return null;
+
+        return JsonVO.success(productService.myList(query));
     }
 
     @DeleteMapping("/delete")
-    @ApiOperation(value = "删除产品列表")
+    @ApiOperation(value = "删除产品（可以批量删除）")
     @Override
     public JsonVO<String> deleteBatchProducts(ProductListDTO dto) {
-        return null;
+        //获取ids
+        List<Long> productIds = dto.getProductIds();
+        //判断ids合法
+        if(productIds == null || productIds.isEmpty()){
+            return JsonVO.fail("Id为空");
+        }
+        productService.myDelete(productIds);
+        return JsonVO.success("删除成功");
     }
 
     @PostMapping("/update")
-    @ApiOperation(value = "查询关联供应商列表")
+    @ApiOperation(value = "修改一个产品的信息")
     @Override
     public JsonVO<String> updateProduct(ProductDTO dto) {
-        return null;
+        productService.myUpdate(dto);
+        return JsonVO.success("修改成功");
     }
 
     @PostMapping("/add")
-    @ApiOperation(value = "查询关联供应商列表")
+    @ApiOperation(value = "添加一个新的产品")
     @Override
-    public JsonVO<String> addProduct(ProductDTO dto) {
-        return null;
+    public JsonVO<String> addProduct(@RequestBody ProductDTO dto) {
+        productService.myAdd(dto);
+        return JsonVO.success("添加成功");
     }
 
-    @GetMapping("/one")
-    @ApiOperation(value = "查询关联供应商列表")
+
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "根据id获得一个产品信息")
     @Override
-    public JsonVO<ProductDTO> queryByID(Long id) {
-        return null;
+    public JsonVO<ProductDTO> queryByID(@PathVariable Long id) {
+        ProductDTO dto =  productService.myGetById(id);
+        return JsonVO.success(dto);
     }
 }
