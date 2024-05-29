c8cf262f7ba75ac92e9c3400cf4e1d3b6288d298
ChengcanGuo <2488700263@qq.com>
2024-05-29 20:33:17 +0800

    重新生成了车间设置service，mapper，entity

---
+++ mes-java/mes-j5-shopsettings/src/main/java/com/zeroone/star/shopsettings/service/IMdWorkshopService.java
@@ -0,0 +1,16 @@
+package com.zeroone.star.shopsettings.service;
+
+import com.zeroone.star.shopsettings.entity.MdWorkshop;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 车间表 服务类
+ * </p>
+ *
+ * @author interstellar
+ * @since 2024-05-29
+ */
+public interface IMdWorkshopService extends IService<MdWorkshop> {
+
+}
---
+++ mes-java/mes-j5-shopsettings/src/main/java/com/zeroone/star/shopsettings/service/impl/MdWorkshopServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zeroone.star.shopsettings.service.impl;
+
+import com.zeroone.star.shopsettings.entity.MdWorkshop;
+import com.zeroone.star.shopsettings.mapper.MdWorkshopMapper;
+import com.zeroone.star.shopsettings.service.IMdWorkshopService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 车间表 服务实现类
+ * </p>
+ *
+ * @author interstellar
+ * @since 2024-05-29
+ */
+@Service
+public class MdWorkshopServiceImpl extends ServiceImpl<MdWorkshopMapper, MdWorkshop> implements IMdWorkshopService {
+
+}
