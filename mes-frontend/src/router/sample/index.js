bfb40a49f6f92b094d954f5799ad3ee5b08aab55
f1-海山了 <2691246230@qq.com>
2024-05-29 19:23:13 +0800

    添加工艺流程的功能实现

--- mes-frontend/src/router/sample/index.js
+++ mes-frontend/src/router/sample/index.js
@@ -44,7 +44,12 @@ const routes = [
 			{
 				path: '/sample/unit-account',
 				component: () => import('@/views/sample/basic-data/unit-of-account.vue')
-			}
+			},
+			 // 车间设置
+			 {
+				path: '/sample/workshop-settings',
+				component: () => import('@/views/sample/workshop/WorkshopSettings.vue')
+			  }
 		]
 	}
 ]
