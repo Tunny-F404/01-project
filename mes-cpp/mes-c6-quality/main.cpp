7b82fbc43fba42953743dc1d0667afdc81e6ab45
Dream_memory <2298901711@qq.com>
2024-05-30 22:38:36 +0800

    修改

--- mes-cpp/mes-c6-quality/main.cpp
+++ mes-cpp/mes-c6-quality/main.cpp
@@ -46,7 +46,7 @@ bool getStartArg(int argc, char* argv[]) {
 	// 数据库连接信息
 	std::string dbUsername = "root";
 	std::string dbPassword = "123456";
-	std::string dbName = "test";
+	std::string dbName = "ktgmes";
 
 	std::string dbHost = "localhost";
 	int dbPort = 3306;
