db4fb59fc5632997791422959e2c0e45295f71ad
XY <2627503031@qq.com>
2024-05-30 21:02:04 +0800

    对功能进行了初步的实现(有bug)

---
+++ mes-java/mes-j4-whsmanagement/src/main/java/com/zeroone/star/wmoutsourcerecpt/constants/userConstants.java
@@ -0,0 +1,11 @@
+package com.zeroone.star.wmoutsourcerecpt.constants;
+
+/**
+ * @Author: jingzi
+ * @createTime: 2024-05-30
+ * @description: TODO
+ */
+
+public class userConstants {
+    public static final String ORDER_STATUS_FINISHED = "FINISHED";
+}
