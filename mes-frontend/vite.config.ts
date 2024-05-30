c875fac1e66628d24a636544bf1dda8e0af5fa3d
ruan-cat <1219043956@qq.com>
2024-05-31 00:03:48 +0800

    🤔 save-file: 保存。正在处理类型声明混乱问题。

--- mes-frontend/vite.config.ts
+++ mes-frontend/vite.config.ts
@@ -1,10 +1,10 @@
-import { fileURLToPath, URL } from "node:url";
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import { createHtmlPlugin } from "vite-plugin-html";
+import { fileURLToPath, URL } from "node:url";
 
 const getViteEnv = (mode, target) => {
 	return loadEnv(mode, process.cwd())[target];
