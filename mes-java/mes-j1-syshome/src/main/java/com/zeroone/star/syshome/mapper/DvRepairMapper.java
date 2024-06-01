2e555d002c86d63e374817588fc86aeb0176bb46
lxh <2337438881@qq.com>
2024-06-01 10:45:35 +0800

    修正sql语句

--- mes-java/mes-j1-syshome/src/main/java/com/zeroone/star/syshome/mapper/DvRepairMapper.java
+++ mes-java/mes-j1-syshome/src/main/java/com/zeroone/star/syshome/mapper/DvRepairMapper.java
@@ -24,32 +24,33 @@ public interface DvRepairMapper extends BaseMapper<DvRepair> {
     @Select("SELECT 'pro_task' AS eventType, '生产任务' AS eventTypeName, t.task_id AS id " +
             "FROM pro_task t " +
             "JOIN pro_user_workstation uw ON t.workstation_id = uw.workstation_id " +
-            "WHERE uw.user_name = t.end_time = NULL AND uw.user_name = #{userName} " +
+            "WHERE t.end_time = NULL AND uw.user_name = '1' " +
             "UNION " +
             "SELECT 'pro_feedback' AS eventType, '生产报工表审批' AS eventTypeName,f.record_id AS id " +
             "FROM pro_feedback f " +
-            "WHERE f.user_name = #{userName} AND (f.status = '审批中' OR f.status = '已确认') " +
+            "WHERE f.user_name = '1' AND (f.status = '审批中' OR f.status = '已确认') " +
             "UNION " +
-            "SELECT 'dv_repair' AS eventType, '设备维修' AS eventTypeName, f.repair_id AS id " +
-            "FROM dv_repair f " +
+            "SELECT 'dv_repair' AS eventType, '设备维修' AS eventTypeName, r.repair_id AS id" +
+            " FROM dv_repair r " +
             "JOIN dv_repair_line rl ON r.repair_id = rl.repair_id " +
-            "WHERE r.accepted_by = #{userName} AND r.finish_date = NULL " +
+            "WHERE r.accepted_by = '1' AND r.finish_date = NULL " +
             "UNION " +
-            "SELECT 'dv_repair' AS eventType, '设备维修检验' AS eventTypeName, f.repair_id AS id " +
-            "FROM dv_repair f JOIN dv_repair_line rl ON r.repair_id = rl.repair_id " +
-            "WHERE r.confirm_by = #{userName} AND r.finish_date = NULL " +
+            "SELECT 'dv_repair' AS eventType, '设备维修检验' AS eventTypeName, r.repair_id AS id " +
+            "FROM dv_repair r " +
+            "JOIN dv_repair_line rl ON r.repair_id = rl.repair_id " +
+            "WHERE r.confirm_by = '1' AND r.finish_date = NULL " +
             "UNION " +
             "SELECT 'qc_ipqc' AS eventType, '过程检验' AS eventTypeName, i.ipqc_id AS id " +
             "FROM qc_ipqc i " +
-            "WHERE i.inspector = #{userName} AND (i.status = '审批中' OR i.status = '已确认') " +
+            "WHERE i.inspector = '1' AND (i.status = '审批中' OR i.status = '已确认') " +
             "UNION " +
             "SELECT 'qc_iqc' AS eventType, '来料检验' AS eventTypeName, i.iqc_id AS id " +
             "FROM qc_iqc i " +
-            "WHERE i.inspector = #{userName} AND (i.status = '审批中' OR i.status = '已确认') " +
+            "WHERE i.inspector = '1' AND (i.status = '审批中' OR i.status = '已确认') " +
             "UNION " +
             "SELECT 'qc_oqc' AS eventType, '出货检验' AS eventTypeName, o.oqc_id AS id " +
             "FROM qc_oqc o " +
-            "WHERE o.inspector = #{userName} AND (o.status = '审批中' OR o.status = '已确认') ")
+            "WHERE o.inspector = '1' AND (o.status = '审批中' OR o.status = '已确认')")
     Page<ToDoEventVO> selectToDoEvent(PageQuery pageQuery, String userName);
 
 }
