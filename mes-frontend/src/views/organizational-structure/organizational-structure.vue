4ae5d8b162f19cb7ce075d4eb232eab4f6fdfa8c
f1-HarryCong <lq087611@163.com>
2024-06-03 00:05:20 +0800

    完成基本页面响应数据定义，树形展示与调用api接口未实现

--- mes-frontend/src/views/organizational-structure/organizational-structure.vue
+++ mes-frontend/src/views/organizational-structure/organizational-structure.vue
@@ -47,7 +47,7 @@
             <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
                 <template #default="{ row }">
                     <el-button type="text" size="small" icon="edit" @click="handleUpdate(row)">修改</el-button>
-                    <el-button type="text" size="small" icon="plus" @click="handleAdd(row)">新增</el-button>
+                    <el-button type="text" size="small" icon="plus" @click="handleAdd()">新增</el-button>
                     <el-button type="text" size="small" icon="delete" @click="handleDelete(row)">删除</el-button>
                 </template>
             </el-table-column>
