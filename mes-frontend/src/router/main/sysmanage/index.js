bc671ceef8a8e21506864e313b581d9557bb34b6
f1-W <wxf1998007@163.com>
2024-06-02 22:54:52 +0800

    生产管理子页面初版

--- mes-frontend/src/router/main/sysmanage/index.js
+++ mes-frontend/src/router/main/sysmanage/index.js
@@ -1,42 +1,4 @@
 const routes = [
-<<<<<<< HEAD
-  {
-    path: '/un-1',
-    name: 'UN1',
-    component: () => import('@/views/personal-center/personal-center.vue')
-  },
-  {
-    path: '/un-2',
-    name: 'UN2',
-    component: () => import('@/views/status/403.vue')
-  },
-  {
-    path: '/un-3',
-    name: 'UN3',
-    component: () => import('@/views/status/404.vue')
-  },
-  {
-    path: '/un-4',
-    name: 'UN4',
-    component: () => import('@/views/status/403.vue')
-  },
-  {
-    path: '/un-5',
-    name: 'UN5',
-    component: () => import('@/views/status/404.vue')
-  },
-  {
-    path: '/un-6',
-    name: 'UN6',
-    component: () => import('@/views/sample/productionmanagement/Processsetting.vue')
-  },
-  {
-    path: '/un-7',
-    name: 'UN7',
-    component: () => import('@/views/sample/station/Station.vue')
-  },
-]
-=======
 	{
 		path: "/un-1",
 		name: "UN1",
@@ -61,7 +23,7 @@ const routes = [
 	{
 		path: "/un-5",
 		name: "UN5",
-		component: () => import("@/views/status/404.vue"),
+		component: () => import("@/views/sample/Productionworkorder/Productionworkorder.vue"),
 	},
 	{
 		path: "/un-6",
@@ -71,7 +33,17 @@ const routes = [
 	{
 		path: "/un-7",
 		name: "UN7",
-		component: () => import("@/views/status/404.vue"),
+		component: () => import("@/views/sample/station/Station.vue"),
+	},
+	{
+		path: "/un-8",
+		name: "UN8",
+		component: () => import("@/views/sample/productionmanagement/Processsetting.vue"),
+	},
+	{
+		path: "/un-9",
+		name: "UN9",
+		component: () => import("@/views/sample/technologicalprocess/technologicalprocess.vue"),
 	},
 	// 仓库管理页面
 	{
@@ -80,6 +52,5 @@ const routes = [
 		component: () => import("@/views/warehouse-management/warehouse-management.vue"),
 	},
 ];
->>>>>>> e4e36d1c3a2a21aee199b48fe70a9a6dbd9e54a7
 
 export default routes;
