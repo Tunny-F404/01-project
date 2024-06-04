fabcbea21d18d5f95bf0bbf9a367736c3e3de316
mumu <i25609@163.com>
2024-06-04 23:55:19 +0800

    系统管理部署版

--- mes-java/mes-j4-sysmanagement/src/main/java/com/zeroone/star/sysmanagement/SysManagementApplication.java
+++ mes-java/mes-j4-sysmanagement/src/main/java/com/zeroone/star/sysmanagement/SysManagementApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * <p>
@@ -16,6 +17,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class SysManagementApplication {
 
     public static void main(String[] args) {
