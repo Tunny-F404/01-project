daebe789a07bcc0dc0a20179589ac93f9332f999
f1-阮喵喵 <1219043956@qq.com>
2024-06-06 09:33:18 +0800

    🎉 init(sample): table-list-use-demo-page 新建测试界面；

---
+++ mes-frontend/src/views/sample/table-list-use/table-list-use-demo-page.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { computed, ref, watch } from "vue";
+
+defineOptions({
+	/** 列表页用的表格组件 演示页 */
+	name: "TableListUseDemoPage",
+});
+</script>
+
+<template>
+	<section class="table-list-use-demo-page-root"></section>
+</template>
+
+<style lang="scss" scoped>
+.table-list-use-demo-page-root {
+}
+</style>
