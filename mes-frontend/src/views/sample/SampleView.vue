04ab232cde4fd0317fda4eb0939518b000e17420
f1-阮喵喵 <1219043956@qq.com>
2024-06-06 13:04:18 +0800

    ✨ feat: sample页面，注册阮喵喵 标准表格组件 演示页

--- mes-frontend/src/views/sample/SampleView.vue
+++ mes-frontend/src/views/sample/SampleView.vue
@@ -107,6 +107,13 @@ const menus = reactive([
 		path: "/sample/table-list",
 		text: "表格1演示页",
 	},
+
+	// 阮喵喵 标准表格组件 演示页
+	{
+		path: "/sample/table-list-use-demo-page",
+		text: "ruancat标准表格演示页面",
+	},
+
 	{
 		path: "/sample/unit-account",
 		text: "计算单位演示页",
